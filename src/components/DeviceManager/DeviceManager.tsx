6d474d232915850a71ccfa756f8ce3a11f1d2ed3
Azat Mardan <azat-co@users.noreply.github.com>
2023-03-29 17:13:24 -0600

    🐛 fix(DeviceManager.tsx): change devices state type to Device<CommonDeviceProperties>[] ✨ feat(DeviceManager.tsx): fetch devices from Seam API and display device_id and name in the list

--- src/components/DeviceManager/DeviceManager.tsx
+++ src/components/DeviceManager/DeviceManager.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from 'react'
-// import Seam from 'seamapi'
+import Seam, { CommonDeviceProperties, Device } from 'seamapi'
 
 export interface DeviceManagerProps {
   label: string
 }
 
 const DeviceManager = (props: DeviceManagerProps) => {
-  const [devices, setDevices] = useState(['1','2'])
+  const [devices, setDevices] = useState<Device<CommonDeviceProperties>[]>([])
+  useEffect(() => {
+    ;(async () => {
+      const seam = new Seam({
+        apiKey: window.SEAM_PUBLISHED_KEY,
+        endpoint: window.SEAM_ENDPOINT,
+      })
+      const devices = await seam.devices.list()
+      setDevices(devices)
+    })()
+  }, [])
   return (
     <>
       <h1>Device Manager</h1>
       <ul>
         {devices.map((device) => (
-          <li key={device}>{device}</li>
+          <li key={device.device_id}>
+            {device.device_id}: {device.properties.name}
+          </li>
         ))}
       </ul>
       <button>{props.label}</button>
