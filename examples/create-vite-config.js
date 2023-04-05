604fcac5acde509950e1b162dc458c0e8220b476
Evan Sosenko <evan@getseam.com>
2023-04-04 21:42:38 -0700

    Do note use ts for vite config

--- examples/create-vite-config.js
+++ examples/create-vite-config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default (url) =>
-  defineConfig({
+export default async (url) =>
+  await defineConfig({
     root: dirname(fileURLToPath(url)),
     plugins: [tsconfigPaths(), react()]
   })
