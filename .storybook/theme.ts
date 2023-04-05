f08dbf35ae34b311dcc6b5fb20e205a02b4c0ddc
Evan Sosenko <evan@getseam.com>
2023-04-05 10:58:31 -0700

    Add seam branded theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Seam',
+  brandUrl: 'https://seam.co',
+  brandImage: '/seam-wordmark-s.webp',
+  brandTarget: '_self'
+})
