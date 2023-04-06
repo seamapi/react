84a36ca6fc21105cf552d7ce265c465482186b65
Azat Mardan <azat-co@users.noreply.github.com>
2023-04-06 15:30:23 -0600

    Update DeviceManager.tsx

--- src/components/DeviceManager/DeviceManager.tsx
+++ src/components/DeviceManager/DeviceManager.tsx
@@ -1,27 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Seam, { CommonDeviceProperties, Device } from 'seamapi'
-const getCookie = (name: string) => {
-  const nameEQ = name + '='
-  const ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i]
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
-  }
-  return null
-}
-const setCookie = (name: string, value: string, days: number) => {
-  let expires = ''
-  if (days) {
-    const date = new Date()
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    expires = '; expires=' + date.toUTCString()
-  }
-  document.cookie = name + '=' + (value || '') + expires + '; path=/'
-}
-const deleteCookie = (name: string) => {
-  document.cookie = name + '=; Max-Age=-99999999;'
-}
+import { getCookie, setCookie } from '../../utils'
 
 export interface DeviceManagerProps {
   clientAccessToken: string
@@ -104,10 +83,10 @@ const DeviceManager = (props: DeviceManagerProps) => {
         {devices.map((device) => (
           <li key={device.device_id}>
             {device.device_id}: {device.properties.name}
+            <button>view device</button>
           </li>
         ))}
       </ul>
-      <button>{props.label}</button>
     </>
   )
 }
