98eb7e56feb4b5128ca0c27d852419ba20b69365
Evan Sosenko <evan@getseam.com>
2023-04-14 16:46:04 -0700

    Do not use args for publishableKey

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,11 @@ import type { Preview } from '@storybook/react'
 
 import { SeamProvider } from 'index.js'
 
+// TODO: UPSTREAM: Use toolbar input when it lands
+// https://github.com/storybookjs/storybook/pull/21959
+// TODO: Use fake-seam-connect endpoint and publishableKey
+const publishableKey = 'seam_pk1fGd41X_zKs0ZELRWEc8nWxiBsrTFC98'
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,14 +17,8 @@ const preview: Preview = {
       }
     }
   },
-  // TODO: UPSTREAM: Use toolbar input when it lands
-  // https://github.com/storybookjs/storybook/pull/21959
-  argTypes: { publishableKey: { control: { type: 'text' } } },
-  // TODO: Use fake-seam-connect endpoint and publishableKey
-  args: { publishableKey: 'seam_pk1fGd41X_zKs0ZELRWEc8nWxiBsrTFC98' },
   decorators: [
     (Story, { args }) => {
-      const publishableKey = args['publishableKey']
       return (
         <SeamProvider publishableKey={publishableKey} endpoint='/api'>
           <Story />
