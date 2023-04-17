36c37d8371db0b1e11ae10f1199232b90f51249b
Azat Mardan <azat-co@users.noreply.github.com>
2023-04-17 07:41:43 -0600

    Update use-seam-client.ts

--- src/hooks/use-seam-client.ts
+++ src/hooks/use-seam-client.ts
@@ -13,26 +13,24 @@ export const useSeamClient = ({
   clientSessionToken?: string
 }): Seam => {
   if (typeof window === 'undefined') return null as any
-
+  if (!endpoint) throw new Error('No endpoint provided')
+  const getSeam = (token: string) => {
+    return new Seam({
+      clientSessionToken: token,
+      endpoint,
+    })
+  }
   const clientSessionTokenFromCookie = getCookie(`seam-clientSessionToken-${userIdentifierKey}`)
   // if clientSessionToken is provided, use it, no need to check cookie or public key (create new client access token)
   if (clientSessionToken?.startsWith('seam_cst')) {
     // backend generated client access token
     setCookie(`seam-clientSessionToken-${userIdentifierKey}`, clientSessionToken, 30)
-    const seam = new Seam({
-      clientSessionToken: clientSessionToken,
-      endpoint,
-    })
-    return seam
+    return getSeam(clientSessionToken)
   } else if (clientSessionTokenFromCookie) {
     // already have client access token from cookie
     // no need to use public key
-    const seam = new Seam({
-      clientSessionToken: clientSessionTokenFromCookie,
-      endpoint,
-    })
     // TODO: check if client access token is valid (not expired), if expired/invalid, get new one with the public key
-    return seam
+    return getSeam(clientSessionTokenFromCookie)
   } else if (publishedKey?.startsWith('seam_pk1')) {
     // public key, need to create a client access token and store it in cookie
     ;(async () => {
@@ -45,11 +43,7 @@ export const useSeamClient = ({
         throw new Error('Could not get client access token')
       }
       setCookie(`seam-clientSessionToken-${userIdentifierKey}`, clientSessionToken, 30)
-      const seam = new Seam({
-        clientSessionToken: clientSessionToken,
-        endpoint: endpoint,
-      })
-      return seam
+      return getSeam(clientSessionToken)
       // TODO: improve: use devices are not loading probably due to async nature of this hook
     })()
   } else {
