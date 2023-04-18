4b29c5a6cb23620f8e7a24149c94325fdaf69c98
Evan Sosenko <evan@getseam.com>
2023-04-18 15:23:06 -0700

    Note upstream issue for middleware.js

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,5 +1,7 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
+// UPSTREAM: https://github.com/storybookjs/storybook/issues/208#issuecomment-1485081586
+
 /** @type {(app: import('express').Router) => void} */
 export default (app) => {
   app.use(
