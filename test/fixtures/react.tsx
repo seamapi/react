567f7a281669b55a4d7e90c18ce5aa4ef249ced0
Evan Sosenko <evan@getseam.com>
2023-04-18 21:46:54 -0700

    Move publishableKey into const in fixture

--- test/fixtures/react.tsx
+++ test/fixtures/react.tsx
@@ -1,15 +1,11 @@
-import { env } from 'node:process'
-
 import { render } from '@testing-library/react'
 import { type ReactElement, type ReactNode } from 'react'
 
 import { SeamProvider } from 'index.js'
 
+const publishableKey = 'seam_pk1fGd41X_zKs0ZELRWEc8nWxiBsrTFC98'
+
 const Providers = ({ children }: { children: ReactNode }): ReactElement => {
-  const publishableKey = env['SEAM_PUBLISHABLE_KEY']
-  if (publishableKey == null) {
-    throw new Error('Missing SEAM_PUBLISHABLE_KEY')
-  }
   return <SeamProvider publishableKey={publishableKey}>{children}</SeamProvider>
 }
 
