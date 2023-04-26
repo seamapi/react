8cac8064d43c6d57a0bda2f668db606cc7bb1bc3
Evan Sosenko <evan@getseam.com>
2023-04-26 18:09:26 +0000

    Remove extra log

--- test/fixtures/api.js
+++ test/fixtures/api.js
@@ -6,6 +6,5 @@ const fake = await create()
 await fake.startServer(9000)
 console.log(`Fake Seam Connect listening on ${fake.server.serverUrl}`)
 process.on('SIGTERM', () => {
-  console.log('xxx')
   process.exit(0)
 })
