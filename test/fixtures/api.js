d4a421894de4c4d418d30f706f8553778b493f14
Evan Sosenko <evan@getseam.com>
2023-04-26 18:10:35 +0000

    Remove extra signal handler

--- test/fixtures/api.js
+++ test/fixtures/api.js
@@ -1,10 +1,6 @@
-import process from 'node:process'
-
 import { create } from '@seamapi/fake-seam-connect'
 
 const fake = await create()
 await fake.startServer(9000)
+// eslint-disable-next-line no-console
 console.log(`Fake Seam Connect listening on ${fake.server.serverUrl}`)
-process.on('SIGTERM', () => {
-  process.exit(0)
-})
