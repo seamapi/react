e027e1f88209946a82f0144013130292528e58a4
Evan Sosenko <evan@getseam.com>
2023-04-25 20:56:14 -0700

    feat: Use seam-connect-fake for local storybook

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,13 +1,15 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
 // UPSTREAM: https://github.com/storybookjs/storybook/issues/208#issuecomment-1485081586
-
 /** @type {(app: import('express').Router) => void} */
-export default (app) => {
+export default async (app) => {
+  const { create } = await import('@seamapi/fake-seam-connect')
+  const fake = await create()
+  await fake.startServer()
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://connect.getseam.com',
+      target: fake.server.serverUrl,
       pathRewrite: { '^/api': '' },
       changeOrigin: true,
     })
