a6e160520b5c149ee73d24d7f79b9dc5779edf35
Evan Sosenko <evan@getseam.com>
2023-04-26 22:15:32 -0700

    Pass props to SeamProvider ini basic example

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -4,7 +4,12 @@ import { DeviceManager, SeamProvider } from 'index.js'
 
 export const App = (): ReactElement => {
   return (
-    <SeamProvider>
+    <SeamProvider
+      // @ts-expect-error https://vitejs.dev/guide/env-and-mode.html#production-replacement
+      endpoint={import.meta.env.SEAM_ENDPOINT}
+      // @ts-expect-error https://vitejs.dev/guide/env-and-mode.html#production-replacement
+      publishableKey={import.meta.env.SEAM_PUBLISHABLE_KEY}
+    >
       <main>
         <h1>Seam Components</h1>
         <DeviceManager />
--- examples/create-vite-config.js
+++ examples/create-vite-config.js
@@ -13,14 +13,4 @@ export default async (url) =>
     base: env.CI ? `/examples/${basename(dirname(fileURLToPath(url)))}` : '/',
     root: dirname(fileURLToPath(url)),
     plugins: [tsconfigPaths(), react()],
-    server: {
-      port: 8080,
-      proxy: {
-        '/api': {
-          target: 'https://connect.getseam.com',
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          changeOrigin: true,
-        },
-      },
-    },
   })
