467cde797e1a6cb5fb5a3ac8088339d42ab6b8df
Evan Sosenko <evan@getseam.com>
2023-04-27 21:26:55 -0700

    Replace <script type="module">

--- .storybook/csp.js
+++ .storybook/csp.js
@@ -9,6 +9,7 @@ const html = data.toString()
 
 const output = html
   .replace(/<script>/g, `<script nonce="${nonce}">`)
+  .replace(/<script type="module">/g, `<script nonce="${nonce}">`)
   .replace(/<style>/g, `<style nonce="${nonce}">`)
 
 await writeFile(index, output)
