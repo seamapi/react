dd04660f8e4b0522c7ad13febed9f32cdf58c6b5
Evan Sosenko <evan@getseam.com>
2023-04-28 11:58:08 -0700

    Use @seamapi/react over index.js as import path

--- test/fixtures/react.tsx
+++ test/fixtures/react.tsx
@@ -1,7 +1,6 @@
+import { SeamProvider } from '@seamapi/react'
 import { render } from '@testing-library/react'
 import type { ReactElement, ReactNode } from 'react'
-
-import { SeamProvider } from 'index.js'
 
 const publishableKey = 'seam_pk1fGd41X_zKs0ZELRWEc8nWxiBsrTFC98'
 
