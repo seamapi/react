f77adce792aaf28aeb8f3d0e0ce6b266f073cd21
Evan Sosenko <evan@getseam.com>
2023-04-27 21:31:25 -0700

    Fix quote

--- .storybook/csp.js
+++ .storybook/csp.js
@@ -11,7 +11,7 @@ const output = html
   .replace(/<script>/g, `<script nonce="${nonce}">`)
   .replace(
     /<script type="module">/g,
-    `<script type="module" "nonce="${nonce}">`
+    `<script type="module" nonce="${nonce}">`
   )
   .replace(/<style>/g, `<style nonce="${nonce}">`)
 
