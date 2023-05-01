061316ea94f75f5afc4fd6a0aa975406dfef2b03
Evan Sosenko <evan@getseam.com>
2023-05-01 16:45:22 -0700

    feat: Add storybook nonce to index.html

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5'
 import CopyPlugin from 'copy-webpack-plugin'
 import sass from 'sass'
 
+import CspPlugin from './webpack-csp.js'
 import webpackTsconfigpaths from './webpack-tsconfigpaths.js'
 
 const config: StorybookConfig = {
@@ -40,6 +41,8 @@ const config: StorybookConfig = {
           patterns: [{ from: `examples/dist`, to: 'examples' }],
         })
       )
+
+      config.plugins.push(new CspPlugin())
     }
 
     return config
