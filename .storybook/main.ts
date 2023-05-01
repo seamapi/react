1032058d914edabc538488545e5f9f7e6b1968e0
Evan Sosenko <evan@getseam.com>
2023-05-01 11:05:52 -0700

    build: Use copy-webpack-plugin over cpy-cli

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
+import { readdirSync } from 'node:fs'
+
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import CopyPlugin from 'copy-webpack-plugin'
 
 import webpackTsconfigpaths from './webpack-tsconfigpaths.js'
 
@@ -20,6 +23,27 @@ const config: StorybookConfig = {
   },
   webpackFinal: async (config) => {
     webpackTsconfigpaths(config)
+
+    if (config?.plugins == null) config.plugins = []
+
+    const examples = readdirSync('examples', { withFileTypes: true })
+      .filter((f) => f.isDirectory())
+      .map((f) => f.name)
+
+    if (examples.length === 0) {
+      throw new Error('Excepted at least one example')
+    }
+
+    for (const example of examples) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            { from: `examples/${example}/dist`, to: 'examples/basic' },
+          ],
+        })
+      )
+    }
+
     return config
   },
 }
