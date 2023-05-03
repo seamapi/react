c36583dc6bad5064363aeedc5b0d2af56a3ae0db
Evan Sosenko <evan@getseam.com>
2023-05-03 16:36:18 -0700

    refactor: Rename FALLBACK_IMAGE_URL to fallbackImageUrl

--- src/lib/ui/DeviceDetails/DeviceImage.tsx
+++ src/lib/ui/DeviceDetails/DeviceImage.tsx
@@ -1,12 +1,12 @@
 import type { LockDevice } from 'seamapi'
 
-const FALLBACK_IMAGE_URL =
-  'https://connect.getseam.com/assets/images/devices/unknown-lock.png'
-
 export function DeviceImage(props: { device: LockDevice }): JSX.Element {
   const { device } = props
 
-  const url = device.properties.image_url ?? FALLBACK_IMAGE_URL
+  const url = device.properties.image_url ?? fallbackImageUrl
 
   return <img src={url} alt={device.properties.name} />
 }
+
+const fallbackImageUrl =
+  'https://connect.getseam.com/assets/images/devices/unknown-lock.png'
