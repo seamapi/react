3abf86b1dc59ede75f0a5a9a7c9f357d8409c87d
Severin Ibarluzea <seveibar@gmail.com>
2023-05-03 04:18:33 +0200

    seed fake

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,10 +1,15 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
+import { seedFake } from './seed-fake.js'
+
 // UPSTREAM: https://github.com/storybookjs/storybook/issues/208#issuecomment-1485081586
 /** @type {(app: import('express').Router) => void} */
 export default async (app) => {
   const { create } = await import('@seamapi/fake-seam-connect')
   const fake = await create()
+
+  seedFake(fake.database.getState())
+
   await fake.startServer()
   app.use(
     '/api',
