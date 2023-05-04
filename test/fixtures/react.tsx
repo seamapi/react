f5f41848fd24abde34a25288fe797aa5386b7c34
Evan Sosenko <evan@getseam.com>
2023-05-03 17:05:44 -0700

    Prefer PropsWithChildren

--- test/fixtures/react.tsx
+++ test/fixtures/react.tsx
@@ -1,6 +1,6 @@
 import { SeamProvider } from '@seamapi/react'
 import { render } from '@testing-library/react'
-import type React = require('react')
+import type { PropsWithChildren } from 'react'
 
 declare global {
   // eslint-disable-next-line no-var
@@ -13,11 +13,7 @@ declare global {
   var JEST_SEAM_CLIENT_SESSION_TOKEN_2: string
 }
 
-const Providers = ({
-  children,
-}: {
-  children: React.ReactNode
-}): JSX.Element => {
+const Providers = ({ children }: PropsWithChildren): JSX.Element => {
   return (
     <SeamProvider
       endpoint={globalThis.JEST_SEAM_ENDPOINT}
