3366538b63ed6d8d131f6fe53c2c6182b41bcb13
Mike Wu <hello@mikewu.dev>
2023-05-12 20:58:21 +0900

    Access code details (#135)
    * WIP - access code table
    
    * Add use access code, and use-lock
    
    * Finish Access Code Details
    
    * Delete useLock
    
    * Remove unused vars
    
    * Remove stub device
    
    * Remove todo
    
    * Add device stub in comment
    
    * Fix style lint
    
    * Stub out using useFakeDevice
    
    * Make outline button transparent

--- src/lib/ui/DeviceDetails/DeviceModel.tsx
+++ src/lib/ui/DeviceDetails/DeviceModel.tsx
@@ -22,10 +22,13 @@ export const getDeviceModel = ({
   properties,
 }: LockDevice): string | undefined => {
   return (
-    properties?.august_metadata?.model ?? properties?.schlage_metadata?.model
+    properties?.august_metadata?.model ??
+    properties?.schlage_metadata?.model ??
+    t.fallbackModel
   )
 }
 
 const t = {
   model: 'Model',
+  fallbackModel: 'Unknown Lock',
 }
