dac10bc63538347f5571021125bc5ecb089835fe
Evan Sosenko <evan@getseam.com>
2023-05-11 23:03:40 -0700

    fix: Must use normalizedParams in queryKey

--- src/lib/seam/devices/use-device.ts
+++ src/lib/seam/devices/use-device.ts
@@ -1,24 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
-import type { Device, SeamError } from 'seamapi'
+import type {
+  CommonDeviceProperties,
+  Device,
+  DeviceGetRequest,
+  DeviceGetResponse,
+  SeamError,
+} from 'seamapi'
 
 import { useSeamClient } from 'lib/seam/use-seam-client.js'
 import type { UseSeamQueryResult } from 'lib/seam/use-seam-query-result.js'
 
 export function useDevice(
-  params:
-    | {
-        device_id: string
-      }
-    | string
-): UseSeamQueryResult<'device', Device<any>> {
-  const objParams = typeof params === 'string' ? { device_id: params } : params
+  params: DeviceGetRequest | string
+): UseSeamQueryResult<'device', Device<CommonDeviceProperties> | null> {
+  const normalizedParams =
+    typeof params === 'string' ? { device_id: params } : params
+
   const { client } = useSeamClient()
-  const { data, ...rest } = useQuery<any, SeamError>({
+  const { data, ...rest } = useQuery<
+    DeviceGetResponse['device'] | null,
+    SeamError
+  >({
     enabled: client != null,
-    queryKey: ['devices', 'get', params],
+    queryKey: ['devices', 'get', normalizedParams],
     queryFn: async () => {
-      if (client == null) return []
-      return await client?.devices.get(objParams)
+      if (client == null) return null
+      return await client?.devices.get(normalizedParams)
     },
   })
 
