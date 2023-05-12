4e21779f09d051cbebec9ed81e780d749cf2e445
Mike Wu <hello@mikewu.dev>
2023-05-12 11:49:52 +0900

    Update src/lib/seam/devices/index.ts
    Co-authored-by: Evan Sosenko <evan@getseam.com>

--- src/lib/seam/devices/index.ts
+++ src/lib/seam/devices/index.ts
@@ -2,7 +2,7 @@ import type { LockDevice } from 'seamapi'
 
 export * from './use-devices.js'
 
-export const getLockModel = ({ properties }: LockDevice) => {
+export const getDeviceModel = ({ properties }: LockDevice): string | undefined => {
   return (
     properties?.august_metadata?.model ?? properties?.schlage_metadata?.model
   )
