137c5df7a9f929d0b1875b55c4f4ca33e2bf6bce
Mike Wu <hello@mikewu.dev>
2023-05-12 20:48:47 +0900

    Stub out using useFakeDevice

--- src/lib/seam/devices/use-device.ts
+++ src/lib/seam/devices/use-device.ts
@@ -4,6 +4,7 @@ import type {
   Device,
   DeviceGetRequest,
   DeviceGetResponse,
+  LockDevice,
   SeamError,
 } from 'seamapi'
 
@@ -33,4 +34,46 @@ export function useDevice(
   })
 
   return { ...rest, device: data }
+}
+
+export function useFakeDevice(_params: DeviceGetRequest) {
+  const device: LockDevice = {
+    device_id: 'f9a9ab36-9e14-4390-a88c-b4c78304c6aa',
+    device_type: 'august_lock',
+    capabilities_supported: ['access_code', 'lock'],
+    properties: {
+      locked: true,
+      online: true,
+      door_open: false,
+      manufacturer: 'august',
+      battery_level: 0.9999532347993827,
+      serial_number: '00000004-992d-45a0-bea1-9128fdcd8d12',
+      august_metadata: {
+        lock_id: 'lock-1',
+        house_id: 'house-1',
+        lock_name: 'FRONT DOOR',
+        has_keypad: true,
+        house_name: 'My House',
+      },
+      supported_code_lengths: [6],
+      name: 'FRONT DOOR',
+      battery: {
+        level: 0.9999532347993827,
+        status: 'full',
+      },
+      image_url:
+        'https://connect.getseam.com/assets/images/devices/august_wifi-smart-lock-3rd-gen_silver_front.png',
+    },
+    location: {
+      timezone: 'America/Los_Angeles',
+      location_name: 'My House',
+    },
+    connected_account_id: '6fc0f854-cb5a-461a-af93-e543ce1f730f',
+    workspace_id: 'a9598de8-e627-4d01-b119-61ab7b9e07bd',
+    created_at: '2023-05-08T22:37:24.206Z',
+    errors: [],
+    warnings: [],
+  }
+
+  return { isLoading: false, device }
 }
