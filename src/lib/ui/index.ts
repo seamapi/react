b30b71b87b1d9bc3af0368bc69019b6a654320e0
Evan Sosenko <evan@getseam.com>
2023-05-11 22:53:05 -0700

    feat: Export main components

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,1 +1,4 @@
+export * from './AccessCodeTable/AccessCodeTable.js'
+export * from './DeviceDetails/DeviceDetails.js'
 export * from './DeviceManager/DeviceManager.js'
+export * from './DeviceTable/DeviceTable.js'
