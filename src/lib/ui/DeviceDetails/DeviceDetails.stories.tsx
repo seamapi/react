5f06b55fda1b5f0262ad6454e740768d112843a6
Evan Sosenko <evan@getseam.com>
2023-05-16 17:51:30 -0700

    docs; Add loader to DeviceDetails story

--- src/lib/ui/DeviceDetails/DeviceDetails.stories.tsx
+++ src/lib/ui/DeviceDetails/DeviceDetails.stories.tsx
@@ -1,11 +1,9 @@
 import { Close as CloseIcon } from '@mui/icons-material'
 import { Button, Dialog, DialogActions, IconButton } from '@mui/material'
 import type { Meta, StoryObj } from '@storybook/react'
+import { Seam } from 'seamapi'
 
-import {
-  DeviceDetails,
-  type DeviceDetailsProps,
-} from 'lib/ui/DeviceDetails/DeviceDetails.js'
+import { DeviceDetails } from 'lib/ui/DeviceDetails/DeviceDetails.js'
 import useToggle from 'lib/use-toggle.js'
 
 /**
@@ -14,44 +12,67 @@ import useToggle from 'lib/use-toggle.js'
 const meta: Meta<typeof DeviceDetails> = {
   title: 'Example/DeviceDetails',
   component: DeviceDetails,
-  args: {
-    deviceId: '6c874099-0a92-4eb1-92a5-993db45efffc',
-  },
   tags: ['autodocs'],
+  loaders: [
+    async ({ globals: { publishableKey, userIdentifierKey } }) => {
+      const res = await Seam.getClientSessionToken({
+        publishableKey,
+        userIdentifierKey,
+      })
+      if (!res.ok || res.client_session?.token == null) {
+        throw new Error('Failed to get client access token')
+      }
+      const client = new Seam({
+        clientSessionToken: res.client_session.token,
+      })
+      const devices = await client.devices.list()
+      return {
+        deviceId: devices[0]?.device_id,
+      }
+    },
+  ],
 }
 
 export default meta
 
 type Story = StoryObj<typeof DeviceDetails>
 
-export const Content: Story = {}
-
-export const InsideModal: Story = {
-  render: InsideModalComponent,
+export const Content: Story = {
+  render: (props, { loaded }) => (
+    <DeviceDetails {...props} deviceId={props.deviceId ?? loaded['deviceId']} />
+  ),
 }
 
-function InsideModalComponent(props: DeviceDetailsProps): JSX.Element {
-  const [open, toggleOpen] = useToggle()
-  return (
-    <>
-      <Button onClick={toggleOpen}>Open Modal</Button>
-      <Dialog open={open} fullWidth maxWidth='sm' onClose={toggleOpen}>
-        <IconButton
-          sx={{
-            position: 'absolute',
-            top: '4px',
-            right: '8px',
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <div className='seam-components'>
-          <DeviceDetails {...props} />
-        </div>
-        <DialogActions sx={{ justifyContent: 'center', marginBottom: '16px' }}>
-          <Button variant='outlined'>Done</Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  )
+export const InsideModal: Story = {
+  render: (props, { loaded }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [open, toggleOpen] = useToggle()
+    return (
+      <>
+        <Button onClick={toggleOpen}>Open Modal</Button>
+        <Dialog open={open} fullWidth maxWidth='sm' onClose={toggleOpen}>
+          <IconButton
+            sx={{
+              position: 'absolute',
+              top: '4px',
+              right: '8px',
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <div className='seam-components'>
+            <DeviceDetails
+              {...props}
+              deviceId={props.deviceId ?? loaded['deviceId']}
+            />
+          </div>
+          <DialogActions
+            sx={{ justifyContent: 'center', marginBottom: '16px' }}
+          >
+            <Button variant='outlined'>Done</Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    )
+  },
 }
