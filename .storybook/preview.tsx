cd15f9a5db45d4a28bb5199e041011a57b6c2a49
Severin Ibarluzea <seve700@gmail.com>
2023-05-17 23:52:01 +0200

    Update .storybook/preview.tsx
    Co-authored-by: Evan Sosenko <evan@getseam.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,7 +5,7 @@ import type { Preview } from '@storybook/react'
 
 const useFake =
   ['1', 'true'].includes(
-    process.env['STORYBOOK_DISABLE_FAKE']?.toLowerCase()
+    process.env['STORYBOOK_DISABLE_FAKE']?.toLowerCase() ?? ''
   ) && process.env['STORYBOOK_SEAM_ENDPOINT'] == null
 
 const preview: Preview = {
