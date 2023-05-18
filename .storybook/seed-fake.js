1e04de1ee9c10688039f7af6631bec551d41acf0
Mike Wu <hello@mikewu.dev>
2023-05-18 11:59:28 +0900

    Device / Access Code - add more fake data

--- .storybook/seed-fake.js
+++ .storybook/seed-fake.js
@@ -30,20 +30,60 @@ export const seedFake = (db) => {
     device_type: 'august_lock',
     name: 'Front Door',
     workspace_id: ws2.workspace_id,
+    properties: {
+      locked: false,
+      online: true,
+      door_open: false,
+      manufacturer: 'august',
+      battery_level: 0.9999532347993827,
+      serial_number: '00000004-992d-45a0-bea1-9128fdcd8d12',
+      august_metadata: {
+        lock_id: 'lock-1',
+        house_id: 'house-1',
+        lock_name: 'FRONT DOOR',
+        has_keypad: true,
+        house_name: 'My House',
+        keypad_battery_level: 'Not Available',
+      },
+      supported_code_lengths: [6],
+      name: 'FRONT DOOR',
+      battery: {
+        level: 0.9999532347993827,
+        status: 'full',
+      },
+      image_url:
+        'https://connect.getseam.com/assets/images/devices/august_wifi-smart-lock-3rd-gen_silver_front.png',
+      image_alt_text: 'August Wifi Smart Lock 3rd Gen, Silver, Front',
+      code_constraints: [],
+    },
   })
 
   db.addAccessCode({
-    code: '1234',
     device_id: device1.device_id,
+    workspace_id: ws2.workspace_id,
     name: "John's Front Door Code",
-    workspace_id: ws2.workspace_id,
+    code: '1234',
+    common_code_key: null,
+    type: 'ongoing',
+    status: 'set',
+    created_at: '2023-05-08T22:38:19.029Z',
+    errors: [],
+    warnings: [],
+    is_managed: true,
   })
 
   db.addAccessCode({
-    code: '1111',
     device_id: device1.device_id,
-    name: "Mary's Front Door Code",
     workspace_id: ws2.workspace_id,
+    name: "Mary's Front Door Code",
+    code: '1111',
+    common_code_key: null,
+    type: 'ongoing',
+    status: 'set',
+    created_at: '2023-05-08T22:38:30.963Z',
+    errors: [],
+    warnings: [],
+    is_managed: true,
   })
 
   const device2 = db.addDevice({
@@ -51,6 +91,61 @@ export const seedFake = (db) => {
     device_type: 'august_lock',
     name: 'Back Door',
     workspace_id: ws2.workspace_id,
+    properties: {
+      locked: false,
+      online: true,
+      door_open: false,
+      manufacturer: 'august',
+      battery_level: 0.9999532347993827,
+      serial_number: '00000004-992d-45a0-bea1-9128fdcd8d12',
+      august_metadata: {
+        lock_id: 'lock-3',
+        house_id: 'house-1',
+        lock_name: 'GARAGE',
+        has_keypad: true,
+        house_name: 'My House',
+        keypad_battery_level: 'Not Available',
+      },
+      supported_code_lengths: [6],
+      name: 'GARAGE',
+      battery: {
+        level: 0.9999532347993827,
+        status: 'full',
+      },
+      image_url:
+        'https://connect.getseam.com/assets/images/devices/august_wifi-smart-lock-3rd-gen_silver_front.png',
+      image_alt_text: 'August Wifi Smart Lock 3rd Gen, Silver, Front',
+    },
+  })
+
+  const device3 = db.addDevice({
+    connected_account_id: ca.connected_account_id,
+    device_type: 'schlage_lock',
+    name: 'Garage',
+    workspace_id: ws2.workspace_id,
+    properties: {
+      locked: false,
+      online: true,
+      manufacturer: 'schlage',
+      battery_level: 0.48,
+      serial_number: 'device-1',
+      schlage_metadata: {
+        model: 'BE489WB',
+        device_id: 'device-1',
+        device_name: 'FRONT DOOR',
+        access_code_length: 4,
+      },
+      supported_code_lengths: [4],
+      max_active_codes_supported: 299,
+      name: 'FRONT DOOR',
+      battery: {
+        level: 0.48,
+        status: 'good',
+      },
+      image_url:
+        'https://connect.getseam.com/assets/images/devices/schlage_sense-smart-deadbolt-with-camelot-trim_front.png',
+      image_alt_text: 'Schlage Sense Smart Deadbolt with Camelot Trim, Front',
+    },
   })
 
   db.addClientSession({
@@ -61,6 +156,7 @@ export const seedFake = (db) => {
     token: 'seam_cst1_0000',
     device1,
     device2,
+    device3,
   })
 }
 
