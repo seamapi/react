97ed67e9ac5877314bc4dfe916090032a0b60eb0
Severin Ibarluzea <seveibar@gmail.com>
2023-05-19 03:37:03 +0200

    wip

--- src/lib/ui/SupportedDevices/SupportedDeviceRow.tsx
+++ src/lib/ui/SupportedDevices/SupportedDeviceRow.tsx
@@ -1,14 +1,20 @@
+import { TableCell } from '../Table/TableCell.js'
+import { TableRow } from '../Table/TableRow.js'
+
 export default () => {
   return (
-    <tr>
-      <td>
-        <img src='https://www.seam.co/_next/image?url=%2Fimg%2Fdevice-db%2Faugust%2Fsmartlocks%2Faugust_wifi-smart-lock_silver_front.png&w=96&q=75' />
-      </td>
-      <td>smartlocks</td>
-      <td>Wi-Fi Smart Lock</td>
-      <td>AUG-SL05-M01-S01</td>
-      <td>Wi-Fi</td>
-      <td>Live</td>
-    </tr>
+    <TableRow>
+      <TableCell>
+        <img
+          style={{ width: 50 }}
+          src='https://www.seam.co/_next/image?url=%2Fimg%2Fdevice-db%2Faugust%2Fsmartlocks%2Faugust_wifi-smart-lock_silver_front.png&w=96&q=75'
+        />
+      </TableCell>
+      <TableCell>smartlocks</TableCell>
+      <TableCell>Wi-Fi Smart Lock</TableCell>
+      <TableCell>AUG-SL05-M01-S01</TableCell>
+      <TableCell>Wi-Fi</TableCell>
+      <TableCell>Live</TableCell>
+    </TableRow>
   )
 }
