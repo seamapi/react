74bb3c7df24af7b1bcfdf147f476398625ca552a
Severin Ibarluzea <seveibar@gmail.com>
2023-05-23 23:43:25 +0200

    readd import

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5'
 import CopyPlugin from 'copy-webpack-plugin'
 import sass from 'sass'
 
+import CspPlugin from './webpack-csp.js'
 import webpackTsconfigpaths from './webpack-tsconfigpaths.js'
 
 const config: StorybookConfig = {
