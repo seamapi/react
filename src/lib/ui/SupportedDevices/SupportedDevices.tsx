48f8522f9d4ccf17b1e6e13db22b2fea61442245
Tristan Brewster <87919558+xplato@users.noreply.github.com>
2023-05-23 10:51:44 -0600

    fix: Lint

--- src/lib/ui/SupportedDevices/SupportedDevices.tsx
+++ src/lib/ui/SupportedDevices/SupportedDevices.tsx
@@ -7,12 +7,10 @@ import type { DeviceModel } from 'lib/ui/SupportedDevices/types.js'
 import SupportedDeviceRow from './SupportedDeviceRow.js'
 import SupportedDevicesHeader from './SupportedDevicesHeader.js'
 
-export interface SupportedDevicesProps {}
-
-export default function SupportedDevices({}: SupportedDevicesProps) {
+export default function SupportedDevices() {
   const { data, isLoading, isError, refetch } = useQuery<{
     data: {
-      device_models: DeviceModel[]
+      device_models?: DeviceModel[]
     }
   }>({
     queryKey: ['supported_devices'],
@@ -36,26 +34,31 @@ export default function SupportedDevices({}: SupportedDevicesProps) {
             <Button
               variant='solid'
               size='small'
-              onClick={async () => await refetch()}
+              onClick={() => {
+                void refetch()
+              }}
             >
               Retry
             </Button>
           </div>
         )}
 
-        {!isLoading && !isError && data?.data?.device_models && (
-          <table className='seam-supported-devices-table'>
-            <SupportedDevicesHeader />
-            <tbody>
-              {data.data.device_models.map((deviceModel) => (
-                <SupportedDeviceRow
-                  key={deviceModel.manufacturer_model_id}
-                  deviceModel={deviceModel}
-                />
-              ))}
-            </tbody>
-          </table>
-        )}
+        {!isLoading &&
+          !isError &&
+          data?.data?.device_models !== null &&
+          Array.isArray(data?.data?.device_models) && (
+            <table className='seam-supported-devices-table'>
+              <SupportedDevicesHeader />
+              <tbody>
+                {data.data.device_models.map((deviceModel) => (
+                  <SupportedDeviceRow
+                    key={deviceModel.manufacturer_model_id}
+                    deviceModel={deviceModel}
+                  />
+                ))}
+              </tbody>
+            </table>
+          )}
       </div>
     </>
   )
