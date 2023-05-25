f5b4501ba8529d9b1205a01fc40c15184e8ad6cd
Mike Wu <hello@mikewu.dev>
2023-05-25 12:16:49 +0900

    Re-run generate

---
+++ src/lib/icons/ChevronRight.tsx
@@ -0,0 +1,32 @@
+import type { SVGProps } from 'react'
+export const ChevronRightIcon = (
+  props: SVGProps<SVGSVGElement>
+): JSX.Element => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={25}
+    height={24}
+    fill='none'
+    {...props}
+  >
+    <mask
+      id='chevron-right_svg__a'
+      width={25}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits='userSpaceOnUse'
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill='#D9D9D9' d='M.333 0h24v24h-24z' />
+    </mask>
+    <g mask='url(#chevron-right_svg__a)'>
+      <path
+        fill='#9DA1A9'
+        d='m9.733 18-1.4-1.4 4.6-4.6-4.6-4.6 1.4-1.4 6 6-6 6Z'
+      />
+    </g>
+  </svg>
+)
---
+++ src/lib/icons/TriangleWarning.tsx
@@ -0,0 +1,32 @@
+import type { SVGProps } from 'react'
+export const TriangleWarningIcon = (
+  props: SVGProps<SVGSVGElement>
+): JSX.Element => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={24}
+    height={24}
+    fill='none'
+    {...props}
+  >
+    <mask
+      id='triangle-warning_svg__a'
+      width={24}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits='userSpaceOnUse'
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill='#D9D9D9' d='M0 0h24v24H0z' />
+    </mask>
+    <g mask='url(#triangle-warning_svg__a)'>
+      <path
+        fill='#F3980F'
+        d='M1 21 12 2l11 19H1Zm11-3a.968.968 0 0 0 .713-.288A.967.967 0 0 0 13 17a.967.967 0 0 0-.287-.712A.968.968 0 0 0 12 16a.965.965 0 0 0-.712.288A.965.965 0 0 0 11 17c0 .283.096.52.288.712A.965.965 0 0 0 12 18Zm-1-3h2v-5h-2v5Z'
+      />
+    </g>
+  </svg>
+)
