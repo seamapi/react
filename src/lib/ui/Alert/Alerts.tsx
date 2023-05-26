8291ca11cf552ef34500a4a0933102315eb7fb7b
Evan Sosenko <evan@getseam.com>
2023-05-26 15:39:27 -0700

    refactor: Prefer PropsWithChildren

--- src/lib/ui/Alert/Alerts.tsx
+++ src/lib/ui/Alert/Alerts.tsx
@@ -1,14 +1,16 @@
 import classNames from 'classnames'
+import type { PropsWithChildren } from 'react'
 
 import { Alert, type AlertProps } from 'lib/ui/Alert/Alert.js'
 
 interface AlertsProps {
   alerts?: AlertProps[]
-  children?: JSX.Element
   className?: string
 }
 
-export function Alerts(props: AlertsProps): JSX.Element | null {
+export function Alerts(
+  props: PropsWithChildren<AlertsProps>
+): JSX.Element | null {
   const { alerts, children, className } = props
 
   if (alerts?.length === 0) return null
