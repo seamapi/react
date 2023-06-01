2857d5c9c90eb46275907efbd42a388f949c6f74
Evan Sosenko <evan@getseam.com>
2023-06-01 15:11:32 -0700

    Wrap custom element in provider

--- src/lib/element.tsx
+++ src/lib/element.tsx
@@ -1,21 +1,79 @@
 import r2wc from '@r2wc/react-to-web-component'
+import { type QueryClient } from '@tanstack/react-query'
+import { type ComponentType } from 'react'
+import type { Container } from 'react-dom'
+
+import {
+  SeamProvider,
+  type SeamProviderPropsWithClientSessionToken,
+  type SeamProviderPropsWithPublishableKey,
+} from 'lib/seam/SeamProvider.js'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var seamQueryClient: QueryClient | undefined
+}
 
 export interface ElementDefinition {
   name: string
   Component: Parameters<typeof r2wc>[0]
-  props?: ElementProps
+  props?: ElementProps<Record<string, object>>
 }
 
-export type ElementProps = Record<
-  string,
-  'string' | 'number' | 'boolean' | 'function' | 'json'
+export type ElementProps<T> = Partial<
+  Record<keyof T, 'string' | 'number' | 'boolean' | 'function' | 'json'>
 >
+
+type ProviderProps = SeamProviderPropsWithPublishableKey &
+  SeamProviderPropsWithClientSessionToken
+
+const providerProps: ElementProps<ProviderProps> = {
+  publishableKey: 'string',
+  userIdentifierKey: 'string',
+  clientSessionToken: 'string',
+  disableCssInjection: 'boolean',
+  disableFontInjection: 'boolean',
+  unminifiyCss: 'boolean',
+}
 
 export const defineCustomElement = ({
   name,
   Component,
   props = {},
 }: ElementDefinition): void => {
-  const element = r2wc(Component, { props })
+  const element = r2wc(withProvider(Component), {
+    props: {
+      ...props,
+      ...providerProps,
+    },
+  })
   globalThis?.customElements?.define(name, element)
+}
+
+function withProvider<P extends JSX.IntrinsicAttributes>(
+  Component: ComponentType<P>
+) {
+  return ({
+    publishableKey,
+    userIdentifierKey,
+    clientSessionToken,
+    disableCssInjection,
+    disableFontInjection,
+    unminifiyCss,
+    container: _container,
+    ...props
+  }: ProviderProps & { container: Container } & P): JSX.Element | null => {
+    return (
+      <SeamProvider
+        publishableKey={publishableKey}
+        userIdentifierKey={userIdentifierKey}
+        clientSessionToken={clientSessionToken}
+        disableCssInjection={disableCssInjection}
+        disableFontInjection={disableFontInjection}
+        unminifiyCss={unminifiyCss}
+      >
+        <Component {...(props as P)} />
+      </SeamProvider>
+    )
+  }
 }
--- src/lib/seam/components/SupportedDeviceTable/SupportedDeviceTable.element.ts
+++ src/lib/seam/components/SupportedDeviceTable/SupportedDeviceTable.element.ts
@@ -1,8 +1,10 @@
 import type { ElementProps } from 'lib/element.js'
 
+import type { SupportedDeviceTableProps } from './SupportedDeviceTable.js'
+
 export const name = 'seam-supported-device-table'
 
-export const props: ElementProps = {
+export const props: ElementProps<SupportedDeviceTableProps> = {
   cannotFilter: 'boolean',
 }
 
