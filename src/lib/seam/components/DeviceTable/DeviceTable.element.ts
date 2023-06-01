0e1b9e58fe983f199590f4dbddfc5e79f33a34a0
Evan Sosenko <evan@getseam.com>
2023-06-01 16:28:04 -0700

    Add DeviceTable as element

---
+++ src/lib/seam/components/DeviceTable/DeviceTable.element.ts
@@ -0,0 +1,9 @@
+import type { ElementProps } from 'lib/element.js'
+
+import type { DeviceTableProps } from './DeviceTable.js'
+
+export const name = 'seam-device-table'
+
+export const props: ElementProps<DeviceTableProps> = {}
+
+export { DeviceTable as Component } from './DeviceTable.js'
--- src/lib/seam/components/elements.ts
+++ src/lib/seam/components/elements.ts
@@ -1,1 +1,2 @@
+export * as DeviceTable from './DeviceTable/DeviceTable.element.js'
 export * as SupportedDeviceTable from './SupportedDeviceTable/SupportedDeviceTable.element.js'
