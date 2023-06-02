35655ce41d8a5c6f13ebfbc7e4720f138578a445
Evan Sosenko <evan@getseam.com>
2023-06-01 20:23:34 -0700

    Add onBack prop

--- src/lib/seam/components/DeviceTable/DeviceTable.element.ts
+++ src/lib/seam/components/DeviceTable/DeviceTable.element.ts
@@ -4,6 +4,8 @@ import type { DeviceTableProps } from './DeviceTable.js'
 
 export const name = 'seam-device-table'
 
-export const props: ElementProps<DeviceTableProps> = {}
+export const props: ElementProps<DeviceTableProps> = {
+  onBack: 'function',
+}
 
 export { DeviceTable as Component } from './DeviceTable.js'
