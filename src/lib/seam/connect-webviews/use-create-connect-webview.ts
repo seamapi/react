532fbf80b5e7fce014b19f9e097569712c6e7b06
Evan Sosenko <evan@getseam.com>
2023-06-05 17:11:07 -0700

    feat: Add useCreateConnectWebview

---
+++ src/lib/seam/connect-webviews/use-create-connect-webview.ts
@@ -0,0 +1,38 @@
+import { useMutation, type UseMutationResult } from '@tanstack/react-query'
+import type {
+  ConnectWebview,
+  ConnectWebviewCreateRequest,
+  SeamError,
+} from 'seamapi'
+
+import { useSeamClient } from 'lib/seam/use-seam-client.js'
+
+export interface UseCreateConnectWebviewParams {
+  willNavigateToWebview?: boolean
+}
+
+export type UseCreateConnectWebviewMutationParams = ConnectWebviewCreateRequest
+
+export function useCreateConnectWebview(
+  params?: UseCreateConnectWebviewParams
+): UseMutationResult<ConnectWebview, SeamError, ConnectWebviewCreateRequest> {
+  const { willNavigateToWebview = false } = params ?? {}
+  const { client } = useSeamClient()
+
+  return useMutation({
+    mutationFn: async (
+      mutationParams: UseCreateConnectWebviewMutationParams
+    ) => {
+      if (client == null) {
+        throw new Error('Missing seam client')
+      }
+
+      return await client.connectWebviews.create(mutationParams)
+    },
+    onSuccess: ({ url }) => {
+      if (willNavigateToWebview) {
+        globalThis.location.href = url
+      }
+    },
+  })
+}
