e113c0a230395eb09ca82767aa1d8bd15d89d0fe
Dawn Ho <dawn.ho@gmail.com>
2023-06-28 10:33:42 -0700

    Update src/lib/seam/components/AccessCodeTable/AccessCodeHealthBar.stories.tsx
    Co-authored-by: Evan Sosenko <evan@getseam.com>

--- src/lib/seam/components/AccessCodeTable/AccessCodeHealthBar.stories.tsx
+++ src/lib/seam/components/AccessCodeTable/AccessCodeHealthBar.stories.tsx
@@ -64,7 +64,7 @@ export const Content: Story = {
               errors: [
                 {
                   error_code: 'account_disconnected',
-                  message: 'AccessCode account has been disconnected.',
+                  message: 'Account Disconnected, you may need to reconnect the account with a new webview.',
                 },
               ],
               warnings: [
