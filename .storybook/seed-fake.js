447563bc65d92efbc5d5000056fcbcaf60af891e
Dawn Ho <dawn.ho@gmail.com>
2023-06-28 12:45:29 -0700

    fix type

--- .storybook/seed-fake.js
+++ .storybook/seed-fake.js
@@ -151,6 +151,7 @@ export const seedFake = (db) => {
     errors: [
       {
         error_code: 'failed_to_set_on_device',
+        is_device_error: true,
         message:
           'An error occurred when we tried to set the access code on the device. We will continue to try and set the code on the device in case the error was temporary.',
       },
