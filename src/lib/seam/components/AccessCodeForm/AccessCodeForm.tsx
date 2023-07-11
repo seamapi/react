6fa4975d8f2c201375894e6d98b72b0e6c73c698
Mike Wu <hello@mikewu.dev>
2023-07-11 20:59:35 +0900

    Clean up time-based components

--- src/lib/seam/components/AccessCodeForm/AccessCodeForm.tsx
+++ src/lib/seam/components/AccessCodeForm/AccessCodeForm.tsx
@@ -1,19 +1,13 @@
 import classNames from 'classnames'
-import { useEffect, useState } from 'react'
-import type { AccessCode } from 'seamapi'
+import { useState } from 'react'
 
-import { getBrowserTimezone, getTimezoneLabel } from 'lib/dates.js'
-import { ChevronRightIcon } from 'lib/icons/ChevronRight.js'
 import { getRandomInt } from 'lib/numbers.js'
 import { useCreateAccessCode } from 'lib/seam/access-codes/use-create-access-code.js'
-import { TimezonePicker } from 'lib/seam/components/AccessCodeForm/TimezonePicker/TimezonePicker.js'
 import { useDevice, type UseDeviceData } from 'lib/seam/devices/use-device.js'
 import { Button } from 'lib/ui/Button.js'
-import { DateTimePicker } from 'lib/ui/DateTimePicker/DateTimePicker.js'
 import { FormField } from 'lib/ui/FormField.js'
 import { InputLabel } from 'lib/ui/InputLabel.js'
 import { ContentHeader } from 'lib/ui/layout/ContentHeader.js'
-import { RadioField } from 'lib/ui/RadioField/RadioField.js'
 import { TextField } from 'lib/ui/TextField/TextField.js'
 import { useToggle } from 'lib/ui/use-toggle.js'
 
@@ -54,16 +48,9 @@ function Content({
 }): JSX.Element {
   const [name, setName] = useState('')
   const [code, setCode] = useState('')
-  const [type, setType] = useState<AccessCode['type']>('ongoing')
   const [codeInputFocused, toggleCodeInputFocused] = useToggle()
-  const [datePickerVisible, setDatePickerVisible] = useState(false)
-  const [timezone, setTimezone] = useState<string>(getBrowserTimezone())
-  const [startDate, setStartDate] = useState<string | null>(null)
-  const [endDate, setEndDate] = useState<string | null>(null)
 
   const createAccessCode = useCreateAccessCode(device)
-
-  const [timezonePickerVisible, toggleTimezonePicker] = useToggle()
 
   const save = (): void => {
     if (name === '' || code === '') {
@@ -86,21 +73,6 @@ function Content({
       }
     )
   }
-
-  // Auto-show date picker screen if we've selected a time_bound Access
-  // Code, but don't have dates
-  useEffect(() => {
-    if (type !== 'time_bound') {
-      return
-    }
-
-    if (startDate != null || endDate != null) {
-      return
-    }
-
-    setDatePickerVisible(true)
-  }, [type, startDate, endDate, setDatePickerVisible])
-
   const nameError = (): string | undefined => {
     if (name.length > 60) {
       return t.overCharacterLimitError
@@ -144,54 +116,6 @@ function Content({
       code.trim().length > 0 &&
       codeError() === undefined &&
       !createAccessCode.isLoading
-    )
-  }
-
-  if (timezonePickerVisible) {
-    return (
-      <TimezonePicker
-        value={timezone}
-        onChange={setTimezone}
-        onClose={toggleTimezonePicker}
-      />
-    )
-  }
-
-  if (datePickerVisible) {
-    return (
-      <div className='seam-schedule-picker'>
-        <ContentHeader
-          title={t.timingTitle}
-          onBack={() => {
-            setDatePickerVisible(false)
-          }}
-        />
-        <div className='seam-content'>
-          <div className='seam-timezone'>
-            <span className='seam-label'>{t.selectedTimezoneLabel}</span>
-            <span className='seam-selected' onClick={toggleTimezonePicker}>
-              {getTimezoneLabel(timezone)}
-              <ChevronRightIcon />
-            </span>
-          </div>
-          <FormField>
-            <InputLabel>{t.startTimeLabel}</InputLabel>
-            <DateTimePicker
-              value={startDate == null ? '' : startDate}
-              onChange={setStartDate}
-              size='large'
-            />
-          </FormField>
-          <FormField>
-            <InputLabel>{t.endTimeLabel}</InputLabel>
-            <DateTimePicker
-              value={endDate == null ? '' : endDate}
-              onChange={setEndDate}
-              size='large'
-            />
-          </FormField>
-        </div>
-      </div>
     )
   }
 
@@ -249,24 +173,6 @@ function Content({
               {t.codeGenerateButton}
             </Button>
           </div>
-        </FormField>
-        <FormField>
-          <InputLabel>{t.timingInputLabel}</InputLabel>
-          <RadioField
-            value={type}
-            onChange={setType}
-            name='type'
-            options={[
-              {
-                label: t.typeOngoingLabel,
-                value: 'ongoing',
-              },
-              {
-                label: t.typeTimeBoundLabel,
-                value: 'time_bound',
-              },
-            ]}
-          />
         </FormField>
         <div className='seam-actions'>
           <Button onClick={onBack}>Cancel</Button>
