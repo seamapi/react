3527d11d4777c8b1d7153c903d8626764c086033
Mike Wu <hello@mikewu.dev>
2023-07-13 12:12:19 +0900

    Update getRandomInt() with default min/max

--- src/lib/numbers.ts
+++ src/lib/numbers.ts
@@ -1,14 +1,7 @@
 export function getRandomInt(
   options: { max?: number; min?: number } = {}
 ): number {
-  const { min, max } = options
-  if (min != null && max != null) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-
-  if (max != null) {
-    return Math.floor(Math.random() * max)
-  }
-
-  return Math.floor(Math.random())
+  const { min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER } =
+    options
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
