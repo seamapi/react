12c54bb21871ec9bb12f2b447d88a0c045d6fbf6
Mike Wu <hello@mikewu.dev>
2023-07-13 12:15:17 +0900

    Re-export AccessCodeForm.element

--- src/lib/seam/components/elements.ts
+++ src/lib/seam/components/elements.ts
@@ -1,4 +1,5 @@
 export * as AccessCodeDetails from './AccessCodeDetails/AccessCodeDetails.element.js'
+export * as AccessCodeForm from './AccessCodeForm/AccessCodeForm.element.js'
 export * as AccessCodeTable from './AccessCodeTable/AccessCodeTable.element.js'
 export * as ConnectAccountButton from './ConnectAccountButton/ConnectAccountButton.element.js'
 export * as DeviceDetails from './DeviceDetails/DeviceDetails.element.js'
