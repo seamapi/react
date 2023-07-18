f5ea46a22c9c7fc1f09c5c5ccbb43eb0d41afbb2
Evan Sosenko <evan@getseam.com>
2023-07-17 21:41:18 -0700

    test: Create QueryClient for each test

--- test/fixtures/react.tsx
+++ test/fixtures/react.tsx
@@ -1,4 +1,5 @@
 import { SeamProvider } from '@seamapi/react'
+import { QueryClient } from '@tanstack/react-query'
 import { render } from '@testing-library/react'
 import type { PropsWithChildren } from 'react'
 
@@ -13,26 +14,45 @@ declare global {
   var JEST_SEAM_CLIENT_SESSION_TOKEN_2: string
 }
 
-function Providers({ children }: PropsWithChildren): JSX.Element {
-  return (
-    <SeamProvider
-      endpoint={globalThis.JEST_SEAM_ENDPOINT}
-      publishableKey={globalThis.JEST_SEAM_PUBLISHABLE_KEY_1}
-      userIdentifierKey='some_user'
-      disableCssInjection
-      disableFontInjection
-    >
-      {children}
-    </SeamProvider>
-  )
-}
-
 type Render = typeof render
 
 const customRender = (
   ui: Parameters<Render>[0],
   options?: Parameters<Render>[1]
-): ReturnType<Render> => render(ui, { wrapper: Providers, ...options })
+): ReturnType<Render> => {
+  const queryClient = createQueryClient()
+  const Providers = createProviders({ queryClient })
+  return render(ui, { wrapper: Providers, ...options })
+}
+
+const createQueryClient = (): QueryClient =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        // UPSTREAM: Prevent "Jest did not exit one second after the test run completed" error message.
+        // https://tanstack.com/query/v4/docs/react/guides/testing#set-cachetime-to-infinity-with-jest
+        cacheTime: Infinity,
+      },
+    },
+  })
+
+const createProviders = ({ queryClient }: { queryClient: QueryClient }) => {
+  return function Providers({ children }: PropsWithChildren): JSX.Element {
+    return (
+      <SeamProvider
+        queryClient={queryClient}
+        endpoint={globalThis.JEST_SEAM_ENDPOINT}
+        publishableKey={globalThis.JEST_SEAM_PUBLISHABLE_KEY_1}
+        userIdentifierKey='some_user'
+        disableCssInjection
+        disableFontInjection
+      >
+        {children}
+      </SeamProvider>
+    )
+  }
+}
 
 // eslint-disable-next-line import/export
 export * from '@testing-library/react'
