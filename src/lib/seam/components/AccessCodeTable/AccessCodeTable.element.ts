d73c0fa849c036daeb7ba03421811919bae203b1
Evan Sosenko <evan@getseam.com>
2023-07-20 17:50:53 -0700

    feat: Add disableSearch to AccessCodeTable (#293)

--- src/lib/seam/components/AccessCodeTable/AccessCodeTable.element.ts
+++ src/lib/seam/components/AccessCodeTable/AccessCodeTable.element.ts
@@ -7,6 +7,7 @@ export const name = 'seam-access-code-table'
 export const props: ElementProps<AccessCodeTableProps> = {
   deviceId: 'string',
   disableLockUnlock: 'boolean',
+  disableSearch: 'boolean',
   accessCodeFilter: 'function',
   accessCodeComparator: 'function',
   onAccessCodeClick: 'function',
