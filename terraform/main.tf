8a7f4e07562da83fe5833502b7dedca060d2ed3b
Evan Sosenko <evan@getseam.com>
2023-08-07 17:30:37 -0700

    Attach zone to R2 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,23 @@ locals {
   id   = 0
 }
 
+data "cloudflare_zone" "main" {
+  name = var.zone_name
+}
+
+locals {
+  zone_id = data.cloudflare_zone.main.id
+}
+
 resource "cloudflare_r2_bucket" "main" {
   account_id = var.cloudflare_account_id
   name       = join("-", [local.org, local.repo, local.id])
   location   = "WNAM"
+}
+
+resource "cloudflare_record" "main" {
+  zone_id = local.zone_id
+  name    = var.subdomain
+  value   = cloudflare_r2_bucket.main.name
+  type    = "CNAME"
 }
