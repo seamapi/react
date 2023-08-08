ec96750ad430c5c9ee7754f55de83bbd91ac0e61
Evan Sosenko <evan@getseam.com>
2023-08-08 16:26:52 -0700

    feat: Serve assets from R2 (#343)

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,18 +9,12 @@ data "cloudflare_zone" "main" {
 }
 
 locals {
-  zone_id = data.cloudflare_zone.main.id
+  zone_id       = data.cloudflare_zone.main.id
+  bucket_domain = "${var.subdomain}.${var.zone_name}"
 }
 
 resource "cloudflare_r2_bucket" "main" {
   account_id = var.cloudflare_account_id
   name       = join("-", [local.org, local.repo, local.id])
   location   = "WNAM"
-}
-
-resource "cloudflare_record" "main" {
-  zone_id = local.zone_id
-  name    = var.subdomain
-  value   = cloudflare_r2_bucket.main.name
-  type    = "CNAME"
 }
