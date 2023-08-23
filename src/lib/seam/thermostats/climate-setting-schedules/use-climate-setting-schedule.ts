9326c21dda8bf6d2ffdeaa9c8b30af7ff83363ed
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-23 11:12:12 -0700

    accept string param

--- src/lib/seam/thermostats/climate-setting-schedules/use-climate-setting-schedule.ts
+++ src/lib/seam/thermostats/climate-setting-schedules/use-climate-setting-schedule.ts
@@ -9,13 +9,19 @@ import type {
 import { useSeamClient } from 'lib/seam/use-seam-client.js'
 import type { UseSeamQueryResult } from 'lib/seam/use-seam-query-result.js'
 
-export type UseClimateSettingScheduleParams = ClimateSettingScheduleGetRequest
+export type UseClimateSettingScheduleParams =
+  | ClimateSettingScheduleGetRequest
+  | string
 
 export type UseClimateSettingScheduleData = ClimateSettingSchedule | null
 
 export function useClimateSettingSchedule(
   params: UseClimateSettingScheduleParams
 ): UseSeamQueryResult<'climateSettingSchedule', UseClimateSettingScheduleData> {
+  const normalizedParams =
+    typeof params === 'string'
+      ? { climate_setting_schedule_id: params }
+      : params
   const { client } = useSeamClient()
 
   const { data, ...rest } = useQuery<
@@ -23,10 +29,17 @@ export function useClimateSettingSchedule(
     SeamError
   >({
     enabled: client != null,
-    queryKey: ['thermostats', 'climate_setting_schedules', 'get', params],
+    queryKey: [
+      'thermostats',
+      'climate_setting_schedules',
+      'get',
+      normalizedParams,
+    ],
     queryFn: async () => {
       if (client == null) return null
-      return await client.thermostats.climateSettingSchedules.get(params)
+      return await client.thermostats.climateSettingSchedules.get(
+        normalizedParams
+      )
     },
   })
 
