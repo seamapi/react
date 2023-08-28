2cf4502e816704aa07772924f5f692db895d165f
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-28 16:09:03 -0700

    Add Switch component (#377)
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    Co-authored-by: Seam Bot <devops@getseam.com>

---
+++ src/lib/ui/Switch/Switch.tsx
@@ -0,0 +1,38 @@
+import classNames from 'classnames'
+
+export interface SwitchProps {
+  checked: boolean
+  onChange: (checked: boolean) => void
+  enableLabel?: boolean
+}
+
+function Switch({
+  checked,
+  onChange,
+  enableLabel = false,
+}: SwitchProps): JSX.Element {
+  return (
+    <div className='seam-switch-container'>
+      {enableLabel && (
+        <label className='seam-switch-label'>{checked ? t.on : t.off}</label>
+      )}
+      <div
+        className={classNames('seam-switch', {
+          'seam-switch-checked': checked,
+        })}
+        onClick={() => {
+          onChange(!checked)
+        }}
+      >
+        <div className='seam-switch-slider' />
+      </div>
+    </div>
+  )
+}
+
+const t = {
+  on: 'On',
+  off: 'Off',
+}
+
+export default Switch
