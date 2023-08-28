62ade83739729d20cd582c4dadf95ef35952c179
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-28 14:57:02 -0700

    make this a controlled component

--- src/lib/ui/Switch/Switch.stories.tsx
+++ src/lib/ui/Switch/Switch.stories.tsx
@@ -10,13 +10,7 @@ const meta: Meta<typeof Switch> = {
 type Story = StoryObj<typeof Switch>
 
 export const Content: Story = {
-  render: ({ label, defaultChecked }) => {
-    return <Switch label={label} defaultChecked={defaultChecked} />
-  },
-  args: {
-    label: true,
-    defaultChecked: false,
-  },
+  render: (props) => <Switch {...props} />,
 }
 
 export default meta
--- src/lib/ui/Switch/Switch.tsx
+++ src/lib/ui/Switch/Switch.tsx
@@ -1,38 +1,25 @@
-import { useState } from 'react'
-
 interface SwitchProps {
-  defaultChecked?: boolean
+  checked: boolean
+  onChange: (checked: boolean) => void
   label?: boolean
-  onChange?: (checked: boolean) => void
 }
 
-function Switch({
-  defaultChecked = false,
-  label = false,
+const Switch = ({
+  checked,
   onChange,
-}: SwitchProps): JSX.Element {
-  const [isChecked, setIsChecked] = useState<boolean>(defaultChecked)
-
-  const handleToggle = (): void => {
-    setIsChecked(!isChecked)
-    if (onChange != null) {
-      onChange(!isChecked)
-    }
-  }
-
-  return (
-    <div className='seam-switch-container'>
-      {label && (
-        <label className='seam-switch-label'>{isChecked ? 'On' : 'Off'}</label>
-      )}
-      <div
-        className={`seam-switch ${isChecked ? 'checked' : ''}`}
-        onClick={handleToggle}
-      >
-        <div className='slider' />
-      </div>
+  label = false,
+}: SwitchProps): JSX.Element => (
+  <div className='seam-switch-container'>
+    {label && (
+      <label className='seam-switch-label'>{checked ? 'On' : 'Off'}</label>
+    )}
+    <div
+      className={`seam-switch ${checked ? 'checked' : ''}`}
+      onClick={() => onChange(!checked)}
+    >
+      <div className='slider' />
     </div>
-  )
-}
+  </div>
+)
 
 export default Switch
