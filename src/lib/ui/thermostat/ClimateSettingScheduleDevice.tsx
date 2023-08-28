5927dd7e19546f6a0683fb5dccdede804830fd74
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-28 16:00:33 -0700

    broke out into a seperate PR

---
+++ src/lib/ui/thermostat/ClimateSettingScheduleDevice.tsx
@@ -0,0 +1,54 @@
+import { isThermostatDevice, type ThermostatDevice } from 'seamapi'
+
+import { ChevronRightIcon } from 'lib/icons/ChevronRight.js'
+import { useDevice } from 'lib/seam/devices/use-device.js'
+import { DeviceImage } from 'lib/ui/device/DeviceImage.js'
+import { ClimateSettingStatus } from 'lib/ui/thermostat/ClimateSettingStatus.js'
+
+export function ClimateSettingDevice({
+  deviceId,
+}: {
+  deviceId: string
+}): JSX.Element | null {
+  const { isLoading, device } = useDevice({
+    device_id: deviceId,
+  })
+
+  if (isLoading) {
+    return null
+  }
+
+  if (device == null) {
+    return null
+  }
+
+  if (!isThermostatDevice(device)) {
+    return null
+  }
+
+  return <Content device={device} />
+}
+
+function Content(props: { device: ThermostatDevice }): JSX.Element {
+  const { device } = props
+
+  return (
+    <div className='seam-climate-setting-schedule-device-bar'>
+      <div className='seam-device-image'>
+        <DeviceImage device={device} />
+      </div>
+      <div className='seam-climate-setting-schedule-device-details'>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '2.5px' }}>
+          <div className='seam-device-name'>{device.properties.name}</div>
+          <div className='seam-device-current-climate-setting'>
+            Current climate:{' '}
+            <ClimateSettingStatus
+              climateSetting={device.properties.current_climate_setting}
+            />
+          </div>
+        </div>
+        <ChevronRightIcon className='chevron' />
+      </div>
+    </div>
+  )
+}
