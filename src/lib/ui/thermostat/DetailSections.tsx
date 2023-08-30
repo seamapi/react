27498c1a37e0c6313cc2d068f3cb68b30ef16eb6
Tristan B <tristan@infi.im>
2023-08-30 10:35:40 -0600

    Remove empty object type

--- src/lib/ui/thermostat/DetailSections.tsx
+++ src/lib/ui/thermostat/DetailSections.tsx
@@ -1,7 +1,5 @@
 import type { PropsWithChildren } from 'react'
 
-export function DetailSections({
-  children,
-}: PropsWithChildren<{}>): JSX.Element {
+export function DetailSections({ children }: PropsWithChildren): JSX.Element {
   return <div className='seam-thermostat-detail-sections'>{children}</div>
 }
