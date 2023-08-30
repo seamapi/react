5750f65b789d6d3ae2ac8a6cbdceb91e5fb4ef11
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-30 16:22:03 -0700

    refactoring to detail sections

--- src/lib/seam/components/ClimateSettingScheduleDetails/ClimateSettingScheduleDetails.tsx
+++ src/lib/seam/components/ClimateSettingScheduleDetails/ClimateSettingScheduleDetails.tsx
@@ -1,16 +1,15 @@
 import classNames from 'classnames'
 import { useState } from 'react'
 
-import { ArrowRightIcon } from 'lib/icons/ArrowRight.js'
-import { ChevronRightIcon } from 'lib/icons/ChevronRight.js'
 import { DeviceDetails } from 'lib/seam/components/DeviceDetails/DeviceDetails.js'
 import { useClimateSettingSchedule } from 'lib/seam/thermostats/climate-setting-schedules/use-climate-setting-schedule.js'
 import { ContentHeader } from 'lib/ui/layout/ContentHeader.js'
-import { ClimateSettingStatus } from 'lib/ui/thermostat/ClimateSettingStatus.js'
-import { Tooltip } from 'lib/ui/Tooltip/Tooltip.js'
 
 import { formatDateAndTime } from 'lib/dates.js'
-import Switch from 'lib/ui/Switch.js'
+import { AccordionRow } from 'lib/ui/layout/AccordionRow.js'
+import { DetailRow } from 'lib/ui/layout/DetailRow.js'
+import { DetailSection } from 'lib/ui/layout/DetailSection.js'
+import { DetailSectionGroup } from 'lib/ui/layout/DetailSectionGroup.js'
 import { ClimateSettingScheduleCard } from './ClimateSettingScheduleCard.js'
 
 export interface ClimateSettingScheduleDetailsProps {
@@ -50,67 +49,32 @@ export function ClimateSettingScheduleDetails({
       className={classNames('seam-climate-setting-schedule-details', className)}
     >
       <ContentHeader title='Climate setting schedule' onBack={onBack} />
-      <ClimateSettingScheduleCard
-        climateSettingScheduleId={climateSettingScheduleId}
-      />
-      <span className='seam-default-setting-message'>
-        {t.defaultSettingMessagePart1}
-        &nbsp;<span className='seam-default-setting'>{t.defaultSetting}</span>
-        &nbsp;
-        {t.defaultSettingMessagePart2}
-      </span>
-      <div className='seam-box'>
-        <div className='seam-content seam-start-end-toggle'>
-          <div>
-            <span className='seam-label'>
-              {t.startEndTime}
-              <Tooltip>{t.startEndTimeTooltip}</Tooltip>
+      <div className='seam-climate-setting-schedule-details-content'>
+        <ClimateSettingScheduleCard
+          climateSettingScheduleId={climateSettingScheduleId}
+        />
+        <span className='seam-default-setting-message'>
+          {t.defaultSettingMessagePart1}
+          &nbsp;<span className='seam-default-setting'>{t.defaultSetting}</span>
+          &nbsp;
+          {t.defaultSettingMessagePart2}
+        </span>
+        <DetailSectionGroup>
+          <DetailSection>
+            <AccordionRow label={t.startEndTime} />
+            <AccordionRow label={t.climateSetting} />
+            <DetailRow label={t.allowManualOverride} />
+          </DetailSection>
+        </DetailSectionGroup>
+
+        <div style={{ height: '16px' }} />
+        <div className='seam-box'>
+          <div className='seam-content seam-creation-date'>
+            <span className='seam-label'>{t.creationDate}</span>
+            <span className='seam-right seam-value'>
+              {formatDateAndTime(climateSettingSchedule.created_at)}
             </span>
-
-            <div>
-              <span className='seam-value'>
-                {`${formatDateAndTime(
-                  climateSettingSchedule.schedule_starts_at
-                )}`}
-
-                <ArrowRightIcon />
-                {`${formatDateAndTime(
-                  climateSettingSchedule.schedule_ends_at
-                )}`}
-              </span>
-            </div>
           </div>
-          <ChevronRightIcon className='chevron' />
-        </div>
-        <div className='seam-content seam-climate-setting-toggle'>
-          <span className='seam-label'>
-            {t.climateSetting} <Tooltip>{t.climateSettingTooltip}</Tooltip>
-          </span>
-          <div className='seam-right'>
-            <ClimateSettingStatus
-              climateSetting={climateSettingSchedule}
-              iconPlacement='right'
-            />
-            <ChevronRightIcon className='chevron' />
-          </div>
-        </div>
-        <div className='seam-content seam-allow-manual-override-toggle'>
-          <span className='seam-label'>
-            {t.allowManualOverride}{' '}
-            <Tooltip>{t.allowManualOverrideTooltip}</Tooltip>
-          </span>
-          <div className='seam-right'>
-            <Switch label />
-          </div>
-        </div>
-      </div>
-      <div style={{ height: '16px' }} />
-      <div className='seam-box'>
-        <div className='seam-content seam-creation-date'>
-          <span className='seam-label'>{t.creationDate}</span>
-          <span className='seam-right seam-value'>
-            {formatDateAndTime(climateSettingSchedule.created_at)}
-          </span>
         </div>
       </div>
     </div>
