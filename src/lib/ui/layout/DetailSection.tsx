9a6176f2048f73ced6aa13effc1808ca2c5a510f
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-31 11:53:54 -0700

    does this work?

--- src/lib/ui/layout/DetailSection.tsx
+++ src/lib/ui/layout/DetailSection.tsx
@@ -15,8 +15,8 @@ export function DetailSection({
   return (
     <div className='seam-detail-section'>
       <div className='seam-detail-label-wrap'>
-        {label && <p className='seam-detail-label'>{label}</p>}
-        {tooltipContent != null && <Tooltip>{tooltipContent}</Tooltip>}
+        {label !== undefined && <p className='seam-detail-label'>{label}</p>}
+        {tooltipContent !== undefined && <Tooltip>{tooltipContent}</Tooltip>}
       </div>
 
       <div className='seam-detail-group'>{children}</div>
