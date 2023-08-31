46be5257ef0e2361e7f5c54feb0a0d19d3807d28
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-31 16:38:59 -0700

    we dont want that

--- src/lib/seam/components/index.ts
+++ src/lib/seam/components/index.ts
@@ -1,6 +1,5 @@
 export * from './AccessCodeDetails/AccessCodeDetails.js'
 export * from './AccessCodeTable/AccessCodeTable.js'
-export * from './ClimateSettingScheduleDetails/ClimateSettingScheduleDetails.js'
 export * from './ConnectAccountButton/ConnectAccountButton.js'
 export * from './DeviceDetails/DeviceDetails.js'
 export * from './DeviceTable/DeviceTable.js'
