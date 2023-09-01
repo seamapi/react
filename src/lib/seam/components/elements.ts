11f3eb5cba64149969a49ed1470181b8851a2a7e
Evan Sosenko <evan@getseam.com>
2023-09-01 14:39:42 -0700

    feat: Export ClimateSettingScheduleTable

--- src/lib/seam/components/elements.ts
+++ src/lib/seam/components/elements.ts
@@ -1,5 +1,6 @@
 export * as AccessCodeDetails from './AccessCodeDetails/AccessCodeDetails.element.js'
 export * as AccessCodeTable from './AccessCodeTable/AccessCodeTable.element.js'
+export * as ClimateSettingScheduleTable from './ClimateSettingScheduleTable/ClimateSettingScheduleTable.element.js'
 export * as ConnectAccountButton from './ConnectAccountButton/ConnectAccountButton.element.js'
 export * as DeviceDetails from './DeviceDetails/DeviceDetails.element.js'
 export * as DeviceTable from './DeviceTable/DeviceTable.element.js'
