2e0d9119b999169948aee9d45a0ee6d3d24330bb
Tristan B <tristan@infi.im>
2023-09-01 12:38:14 -0600

    Add `TemperatureControlGroup`

---
+++ src/lib/ui/thermostat/TemperatureControlGroup.stories.tsx
@@ -0,0 +1,57 @@
+import { Box } from '@mui/material'
+import type { Meta, StoryObj } from '@storybook/react'
+import { TemperatureControlGroup } from './TemperatureControlGroup.js'
+
+const meta: Meta<typeof TemperatureControlGroup> = {
+  title: 'Library/TemperatureControlGroup',
+  tags: ['autodocs'],
+  component: TemperatureControlGroup,
+}
+
+type Story = StoryObj<typeof TemperatureControlGroup>
+
+export const Content: Story = {
+  render: () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          flexDirection: 'column',
+          gap: "64px",
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            padding: '16px',
+            backgroundColor: '#f7f7f7',
+          }}
+        >
+          <TemperatureControlGroup mode='heatcool' />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            padding: '16px',
+            backgroundColor: '#f7f7f7',
+          }}
+        >
+          <TemperatureControlGroup mode='heat' />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            padding: '16px',
+            backgroundColor: '#f7f7f7',
+          }}
+        >
+          <TemperatureControlGroup mode='cool' />
+        </Box>
+      </Box>
+    )
+  },
+}
+
+export default meta
---
+++ src/lib/ui/thermostat/TemperatureControlGroup.tsx
@@ -0,0 +1,51 @@
+import { ThermostatCoolLargeIcon } from 'lib/icons/ThermostatCoolLarge.js'
+import { ThermostatHeatLargeIcon } from 'lib/icons/ThermostatHeatLarge.js'
+import { TemperatureControl } from 'lib/ui/thermostat/TemperatureControl.js'
+import { useEffect, useState } from 'react'
+
+interface TemperatureControlGroupProps {
+  mode: 'heat' | 'cool' | 'heatcool'
+  delta?: number
+}
+
+export function TemperatureControlGroup({
+  mode,
+  delta = 5,
+}: TemperatureControlGroupProps): JSX.Element {
+  const [heat, setHeat] = useState(75)
+  const [cool, setCool] = useState(70)
+
+  const showHeat = mode === 'heat' || mode === 'heatcool'
+  const showCool = mode === 'cool' || mode === 'heatcool'
+
+  useEffect(() => {
+    if (heat < cool + delta) setCool(heat - delta)
+    
+  }, [heat, delta])
+
+  return (
+    <div className='seam-temperature-control-group'>
+      {showHeat && (
+        <div className='seam-temperature-control-group-block'>
+          <ThermostatHeatLargeIcon />
+          <TemperatureControl
+            variant='heat'
+            value={heat}
+            onChange={(t) => setHeat(t)}
+          />
+        </div>
+      )}
+
+      {showCool && (
+        <div className='seam-temperature-control-group-block'>
+          <ThermostatCoolLargeIcon />
+          <TemperatureControl
+            variant='cool'
+            value={cool}
+            onChange={(t) => setCool(t)}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
