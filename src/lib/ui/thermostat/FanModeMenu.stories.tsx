bffccd97bfe461b761abbf9cca0f02da86d5dad5
Tristan B <tristan@infi.im>
2023-09-01 09:48:18 -0600

    Make controlled

--- src/lib/ui/thermostat/FanModeMenu.stories.tsx
+++ src/lib/ui/thermostat/FanModeMenu.stories.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { FanModeMenu } from './FanModeMenu.js'
+import { useState } from 'react'
 
 const meta: Meta<typeof FanModeMenu> = {
   title: 'Library/FanModeMenu',
@@ -13,9 +14,16 @@ type Story = StoryObj<typeof FanModeMenu>
 
 export const Content: Story = {
   render: () => {
+    const [mode, setMode] = useState<'auto' | 'on'>('auto')
+
     return (
-      <Box display='grid' gap={4} gridTemplateColumns='repeat(4, min-content)' height={400}>
-        <FanModeMenu />
+      <Box
+        display='grid'
+        gap={4}
+        gridTemplateColumns='repeat(4, min-content)'
+        height={400}
+      >
+        <FanModeMenu mode={mode} onChange={(mode) => setMode(mode)} />
       </Box>
     )
   },
--- src/lib/ui/thermostat/FanModeMenu.tsx
+++ src/lib/ui/thermostat/FanModeMenu.tsx
@@ -4,19 +4,15 @@ import { FanIcon } from 'lib/icons/Fan.js'
 import { FanOutlineIcon } from 'lib/icons/FanOutline.js'
 import { Menu } from 'lib/ui/Menu/Menu.js'
 import { MenuItem } from 'lib/ui/Menu/MenuItem.js'
-import { useState } from 'react'
 
 type Mode = 'auto' | 'on'
 
-interface FanModeMenuProps {}
+interface FanModeMenuProps {
+  mode: Mode
+  onChange: (mode: Mode) => void
+}
 
-export function FanModeMenu() {
-  const [currentMode, setCurrentMode] = useState('auto')
-
-  const handleModeChange = (mode: 'auto' | 'on') => {
-    setCurrentMode(mode)
-  }
-
+export function FanModeMenu({ mode, onChange }: FanModeMenuProps): JSX.Element {
   return (
     <Menu
       renderButton={({ onOpen }) => (
@@ -24,13 +20,13 @@ export function FanModeMenu() {
           <div className='seam-fan-mode-menu-button-block'>
             <FanOutlineIcon />
             <span className='seam-fan-mode-menu-button-text'>
-              {currentMode === 'auto' ? t.auto : t.on}
+              {mode === 'auto' ? t.auto : t.on}
             </span>
           </div>
           <ChevronDownIcon />
         </button>
       )}
-      verticalOffset={-170}
+      verticalOffset={-180}
       horizontalOffset={-32}
       backgroundProps={{
         className: 'seam-fan-mode-menu-bg',
@@ -38,13 +34,13 @@ export function FanModeMenu() {
     >
       <Option
         mode='auto'
-        isSelected={currentMode === 'auto'}
-        onClick={() => handleModeChange('auto')}
+        isSelected={mode === 'auto'}
+        onClick={() => onChange('auto')}
       />
       <Option
         mode='on'
-        isSelected={currentMode === 'on'}
-        onClick={() => handleModeChange('on')}
+        isSelected={mode === 'on'}
+        onClick={() => onChange('on')}
       />
     </Menu>
   )
