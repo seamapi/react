3c03496408621379349871a515814f78c964788a
Tristan B <tristan@infi.im>
2023-09-01 09:45:47 -0600

    Add `FanModeMenu`

---
+++ src/lib/ui/thermostat/FanModeMenu.stories.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { FanModeMenu } from './FanModeMenu.js'
+
+const meta: Meta<typeof FanModeMenu> = {
+  title: 'Library/FanModeMenu',
+  tags: ['autodocs'],
+  component: FanModeMenu,
+}
+
+type Story = StoryObj<typeof FanModeMenu>
+
+export const Content: Story = {
+  render: () => {
+    return (
+      <Box display='grid' gap={4} gridTemplateColumns='repeat(4, min-content)' height={400}>
+        <FanModeMenu />
+      </Box>
+    )
+  },
+}
+
+export default meta
---
+++ src/lib/ui/thermostat/FanModeMenu.tsx
@@ -0,0 +1,78 @@
+import { CheckBlackIcon } from 'lib/icons/CheckBlack.js'
+import { ChevronDownIcon } from 'lib/icons/ChevronDown.js'
+import { FanIcon } from 'lib/icons/Fan.js'
+import { FanOutlineIcon } from 'lib/icons/FanOutline.js'
+import { Menu } from 'lib/ui/Menu/Menu.js'
+import { MenuItem } from 'lib/ui/Menu/MenuItem.js'
+import { useState } from 'react'
+
+type Mode = 'auto' | 'on'
+
+interface FanModeMenuProps {}
+
+export function FanModeMenu() {
+  const [currentMode, setCurrentMode] = useState('auto')
+
+  const handleModeChange = (mode: 'auto' | 'on') => {
+    setCurrentMode(mode)
+  }
+
+  return (
+    <Menu
+      renderButton={({ onOpen }) => (
+        <button onClick={onOpen} className='seam-fan-mode-menu-button'>
+          <div className='seam-fan-mode-menu-button-block'>
+            <FanOutlineIcon />
+            <span className='seam-fan-mode-menu-button-text'>
+              {currentMode === 'auto' ? t.auto : t.on}
+            </span>
+          </div>
+          <ChevronDownIcon />
+        </button>
+      )}
+      verticalOffset={-170}
+      horizontalOffset={-32}
+      backgroundProps={{
+        className: 'seam-fan-mode-menu-bg',
+      }}
+    >
+      <Option
+        mode='auto'
+        isSelected={currentMode === 'auto'}
+        onClick={() => handleModeChange('auto')}
+      />
+      <Option
+        mode='on'
+        isSelected={currentMode === 'on'}
+        onClick={() => handleModeChange('on')}
+      />
+    </Menu>
+  )
+}
+
+interface OptionProps {
+  mode: Mode
+  isSelected: boolean
+  onClick: () => void
+}
+
+function Option({ mode, isSelected, onClick }: OptionProps) {
+  return (
+    <MenuItem onClick={onClick}>
+      <div className='seam-fan-mode-menu-item'>
+        <div className='seam-fan-mode-menu-item-block'>
+          <FanIcon />
+          <span>{mode === 'auto' ? t.auto : t.on}</span>
+        </div>
+        <div className='seam-fan-mode-menu-item-block'>
+          {isSelected && <CheckBlackIcon />}
+        </div>
+      </div>
+    </MenuItem>
+  )
+}
+
+const t = {
+  auto: 'Auto',
+  on: 'On',
+}
