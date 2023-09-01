c8e01ae752c7a5088051cb68b961c0217c5d3dd6
Tristan B <tristan@infi.im>
2023-09-01 14:37:09 -0600

    Add `useConnectedAccount`

---
+++ src/lib/seam/connected-accounts/use-connected-account.ts
@@ -0,0 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
+import type {
+  ConnectedAccount,
+  ConnectedAccountsGetRequest,
+  ConnectedAccountsGetResponse,
+  SeamError,
+} from 'seamapi'
+
+import { useSeamClient } from 'lib/seam/use-seam-client.js'
+import type { UseSeamQueryResult } from 'lib/seam/use-seam-query-result.js'
+
+export type UseConnectedAccountData = ConnectedAccount | null
+
+export function useConnectedAccount(
+  params: ConnectedAccountsGetRequest
+): UseSeamQueryResult<'connected_account', UseConnectedAccountData> {
+  const normalizedParams =
+    typeof params === 'string' ? { connected_account_id: params } : params
+
+  const { client } = useSeamClient()
+  const { data, ...rest } = useQuery<
+    ConnectedAccountsGetResponse['connected_account'] | null,
+    SeamError
+  >({
+    enabled: client != null,
+    queryKey: ['devices', 'get', normalizedParams],
+    queryFn: async () => {
+      if (client == null) return null
+      return await client.connectedAccounts.get(normalizedParams)
+    },
+  })
+
+  return { ...rest, connected_account: data }
+}
