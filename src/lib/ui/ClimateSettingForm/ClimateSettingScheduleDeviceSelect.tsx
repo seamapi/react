9ae3a55c464f681bf4066f67550d1c70d1d7c49c
Michael Anderjaska <m.anderjaska@gmail.com>
2023-09-06 17:19:36 -0700

    move out of render body

--- src/lib/ui/ClimateSettingForm/ClimateSettingScheduleDeviceSelect.tsx
+++ src/lib/ui/ClimateSettingForm/ClimateSettingScheduleDeviceSelect.tsx
@@ -6,19 +6,19 @@ interface ClimateSettingScheduleDeviceSelectProps {
   onSelect: (deviceId: string) => void
 }
 
+const deviceFilter = (
+  device: UseDevicesData[number],
+  searchInputValue: string
+): boolean => {
+  if (!isThermostatDevice(device)) return false
+  const value = searchInputValue.trim()
+  if (value === '') return true
+  return device.properties.name.toLowerCase().includes(value)
+}
+
 export function ClimateSettingScheduleDeviceSelect({
   onSelect,
 }: ClimateSettingScheduleDeviceSelectProps): JSX.Element {
-  const deviceFilter = (
-    device: UseDevicesData[number],
-    searchInputValue: string
-  ): boolean => {
-    if (!isThermostatDevice(device)) return false
-    const value = searchInputValue.trim()
-    if (value === '') return true
-    return device.properties.name.toLowerCase().includes(value)
-  }
-
   return (
     <DeviceTable
       deviceFilter={deviceFilter}
