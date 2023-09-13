1a2159c0b69d8ac3ebf66b9d6fef0f5546c2e8b6
Michael Anderjaska <m.anderjaska@gmail.com>
2023-09-12 17:54:32 -0700

    better name

---
+++ src/lib/ui/ClimateSettingForm/ClimateSettingScheduleFormDateAndSchedule.tsx
@@ -0,0 +1,74 @@
+import type { Control, FieldValues } from 'react-hook-form'
+import { Controller } from 'react-hook-form'
+
+import classNames from 'classnames'
+import { DateTimePicker } from 'lib/ui/DateTimePicker/DateTimePicker.js'
+import { FormField } from 'lib/ui/FormField.js'
+import { InputLabel } from 'lib/ui/InputLabel.js'
+import { TextField } from 'lib/ui/TextField/TextField.js'
+
+interface ClimateSettingScheduleFormDateAndScheduleProps {
+  control: Control<FieldValues, any>
+  className?: string
+}
+
+export function ClimateSettingScheduleFormDateAndSchedule({
+  control,
+  className,
+}: ClimateSettingScheduleFormDateAndScheduleProps): JSX.Element {
+  return (
+    <div
+      className={classNames(
+        'climate-setting-schedule-form-name-and-schedule',
+        className
+      )}
+    >
+      <div className='seam-content'>
+        <FormField>
+          <InputLabel>{t.nameInputLabel}</InputLabel>
+          <Controller
+            name='name'
+            control={control}
+            rules={{ maxLength: 10 }}
+            defaultValue=''
+            render={({ field, fieldState: { invalid, error } }) => {
+              return (
+                <TextField
+                  {...field}
+                  size='large'
+                  clearable
+                  hasError={invalid}
+                  helperText={error?.message}
+                />
+              )
+            }}
+          />
+        </FormField>
+        <FormField>
+          <InputLabel>{t.startTimeLabel}</InputLabel>
+          <Controller
+            name='startDate'
+            control={control}
+            defaultValue=''
+            render={({ field }) => <DateTimePicker {...field} size='large' />}
+          />
+        </FormField>
+        <FormField>
+          <InputLabel>{t.endTimeLabel}</InputLabel>
+          <Controller
+            name='endDate'
+            control={control}
+            defaultValue=''
+            render={({ field }) => <DateTimePicker {...field} size='large' />}
+          />
+        </FormField>
+      </div>
+    </div>
+  )
+}
+
+const t = {
+  nameInputLabel: 'Name the climate setting',
+  startTimeLabel: 'Start time',
+  endTimeLabel: 'End time',
+}
