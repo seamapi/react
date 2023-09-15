9a9392abfb2f0a3ca6655fe436912500c39c6e6c
Evan Sosenko <evan@getseam.com>
2023-09-15 03:49:38 +0000

    Set displayName for withRequiredCommonProps

--- src/lib/seam/components/common-props.tsx
+++ src/lib/seam/components/common-props.tsx
@@ -13,8 +13,16 @@ export type CommonProps = Partial<RequiredCommonProps>
 
 export function withRequiredCommonProps<
   P extends CommonProps & JSX.IntrinsicAttributes,
->(Component: ComponentType<P>) {
-  return function (props: P & RequiredCommonProps): JSX.Element | null {
+>(
+  Component: ComponentType<P>
+): (props: P & RequiredCommonProps) => JSX.Element | null {
+  function WithRequiredCommonProps(
+    props: P & RequiredCommonProps
+  ): JSX.Element | null {
     return <Component {...props} />
   }
+  WithRequiredCommonProps.displayName = `WithRequiredCommonProps(${
+    Component.displayName ?? Component.name ?? 'Component'
+  })`
+  return WithRequiredCommonProps
 }
