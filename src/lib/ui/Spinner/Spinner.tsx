9a8304134b0122658a6d7d81b05840b6c5e47015
Tristan B <tristan@infi.im>
2023-09-22 09:44:44 -0600

    Add `Spinner`

---
+++ src/lib/ui/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import classNames from 'classnames'
+
+interface SpinnerProps {
+  size?: 'small' | 'medium' | 'large'
+}
+
+export function Spinner({ size }: SpinnerProps) {
+  return <div className={classNames('seam-spinner', size)}></div>
+}
