ad0268109ccead1fac3554fce3aad00f27dd4bf4
Evan Sosenko <evan@getseam.com>
2023-10-13 15:30:50 -0700

    Setup fake devicedb for storybook

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,3 +1,6 @@
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
 import { seedFake } from './seed-fake.js'
@@ -9,6 +12,16 @@ export default async (app) => {
   const { createFake } = await import('@seamapi/fake-seam-connect')
   const fake = await createFake()
   seedFake(fake.database)
+
+  const fakeDevicedb = await getFakeDevicedb()
+  const fakeDevicedbUrl = fakeDevicedb.serverUrl
+  if (fakeDevicedbUrl == null) throw new Error('Missing fake devicedb url')
+  fake.database.setDevicedbConfig({
+    url: fakeDevicedbUrl,
+    vercelProtectionBypassSecret:
+      fakeDevicedb.database.vercel_protection_bypass_secret,
+  })
+
   await fake.startServer()
 
   // eslint-disable-next-line no-console
@@ -29,4 +42,19 @@ export default async (app) => {
       target: 'http://localhost:8080',
     })
   )
+}
+
+const getFakeDevicedb = async () => {
+  const { createFake } = await import('@seamapi/fake-devicedb')
+  const seed = await readFile(
+    fileURLToPath(new URL('devicedb-seed.json', import.meta.url))
+  )
+  const fake = await createFake()
+  await fake.loadJSON(JSON.parse(seed.toString()))
+  await fake.startServer()
+
+  // eslint-disable-next-line no-console
+  console.log(`Fake Devicedb server running at: "${fake.serverUrl}"`)
+
+  return fake
 }
