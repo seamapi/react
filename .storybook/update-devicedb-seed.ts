d20c7dc0a7bda0ae41c0d2c9f08d51e50d206bb6
Evan Sosenko <evan@getseam.com>
2023-10-13 15:13:11 -0700

    Add storybook seed for devicedb

---
+++ .storybook/update-devicedb-seed.ts
@@ -0,0 +1,44 @@
+import { fileURLToPath } from 'node:url'
+
+import {
+  createFake,
+  type Database,
+  seedDatabaseFromApi,
+} from '@seamapi/fake-devicedb'
+import { SeamHttp } from '@seamapi/http/connect'
+import { writeFile } from 'fs/promises'
+
+const main = async (): Promise<void> => {
+  const publishableKey = 'seam_pk1J0Bgui_oYEuzDhOqUzSBkrPmrNsUuKL'
+  const userIdentifierKey = ' 7ff81ed1-51d4-4e77-9c8d-6f2b8ff8fc35'
+
+  const fake = await createFake()
+  await seedDatabase(fake.database, publishableKey, userIdentifierKey)
+
+  const json = await fake.toJSON()
+
+  await writeFile(
+    fileURLToPath(new URL('devicedb-seed.json', import.meta.url)),
+    JSON.stringify(json, null, 2)
+  )
+}
+
+const seedDatabase = async (
+  db: Database,
+  publishableKey: string,
+  userIdentifierKey: string
+): Promise<void> => {
+  const seam = await SeamHttp.fromPublishableKey(
+    publishableKey,
+    userIdentifierKey
+  )
+  const baseURL = seam.client.defaults.baseURL
+  seam.client.defaults.baseURL = new URL(
+    '/internal/devicedb',
+    baseURL
+  ).toString()
+
+  await seedDatabaseFromApi(db, seam.client)
+}
+
+await main()
