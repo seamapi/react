aaf78b23022d0ea02c4ab59b3ede796d1dc50fc8
Evan Sosenko <evan@getseam.com>
2023-10-19 11:28:06 -1000

    Add fakeDevicedb to test fixture

--- test/fixtures/api.ts
+++ test/fixtures/api.ts
@@ -1,3 +1,4 @@
+import { createFake as createFakeDevicedb } from '@seamapi/fake-devicedb'
 import { createFake } from '@seamapi/fake-seam-connect'
 import { beforeEach } from 'vitest'
 
@@ -10,9 +11,21 @@ export interface ApiTestContext {
 
 beforeEach<ApiTestContext>(async (ctx) => {
   const fake = await createFake()
+  const seed = await seedFake(fake.database)
+
+  const fakeDevicedb = await createFakeDevicedb()
+  await fakeDevicedb.seed()
+  await fakeDevicedb.startServer()
+  const fakeDevicedbUrl = fakeDevicedb.serverUrl
+  if (fakeDevicedbUrl == null) throw new Error('Missing fake devicedb url')
+  fake.database.setDevicedbConfig({
+    url: fakeDevicedbUrl,
+    vercelProtectionBypassSecret:
+      fakeDevicedb.database.vercel_protection_bypass_secret,
+  })
+
   await fake.startServer()
   const endpoint = fake.serverUrl
-  const seed = await seedFake(fake.database)
 
   if (endpoint == null) throw new Error('Fake endpoint is null')
   const res = await fetch(`${endpoint}/health`)
