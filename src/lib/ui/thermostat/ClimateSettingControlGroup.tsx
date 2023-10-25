bf8783b9c2dc4a7dfb612e56ce4dd9052875d9b0
Michael Anderjaska <m.anderjaska@gmail.com>
2023-10-25 19:58:09 +0100

    css done right

---
+++ src/lib/ui/thermostat/ClimateSettingControlGroup.tsx
@@ -0,0 +1,52 @@
+import { ClimateModeMenu } from 'lib/ui/thermostat/ClimateModeMenu.js'
+import { TemperatureControlGroup } from 'lib/ui/thermostat/TemperatureControlGroup.js'
+import type { HvacModeSetting } from 'seamapi'
+
+interface ClimateSettingControlGroupProps {
+  mode: HvacModeSetting
+  onModeChange: (mode: HvacModeSetting) => void
+  heatValue: number
+  onHeatValueChange: (temperature: number) => void
+  coolValue: number
+  onCoolValueChange: (temperature: number) => void
+  delta: number
+  minHeat: number
+  maxHeat: number
+  minCool: number
+  maxCool: number
+}
+
+export function ClimateSettingControlGroup({
+  mode,
+  onModeChange,
+  heatValue,
+  onHeatValueChange,
+  coolValue,
+  onCoolValueChange,
+  delta,
+  minHeat,
+  maxHeat,
+  minCool,
+  maxCool,
+}: ClimateSettingControlGroupProps): JSX.Element {
+  return (
+    <div className='seam-climate-setting-control-group'>
+      <ClimateModeMenu mode={mode} onChange={onModeChange} />
+
+      {mode !== 'off' && (
+        <TemperatureControlGroup
+          coolValue={coolValue}
+          heatValue={heatValue}
+          delta={delta}
+          maxCool={maxCool}
+          maxHeat={maxHeat}
+          minCool={minCool}
+          minHeat={minHeat}
+          mode={mode}
+          onCoolValueChange={onCoolValueChange}
+          onHeatValueChange={onHeatValueChange}
+        />
+      )}
+    </div>
+  )
+}
