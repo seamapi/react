3dd503da2e65fd8c745802b35b7a463d846917b9
Michael Anderjaska <m.anderjaska@gmail.com>
2023-10-27 16:14:44 +0100

    Create Climate Setting Schedule: Set default climate setting if not already set (#542)
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    Co-authored-by: Seam Bot <devops@getseam.com>

--- src/lib/ui/ClimateSettingForm/ClimateSettingScheduleForm.tsx
+++ src/lib/ui/ClimateSettingForm/ClimateSettingScheduleForm.tsx
@@ -1,10 +1,17 @@
 import classNames from 'classnames'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import type { ClimateSetting, HvacModeSetting } from 'seamapi'
+import {
+  type ClimateSetting,
+  type HvacModeSetting,
+  isThermostatDevice,
+} from 'seamapi'
+
+import { useDevice } from 'lib/index.js'
 
 import { getSystemTimeZone } from 'lib/dates.js'
 import { ClimateSettingScheduleFormClimateSetting } from 'lib/ui/ClimateSettingForm/ClimateSettingScheduleFormClimateSetting.js'
+import { ClimateSettingScheduleFormDefaultClimateSetting } from 'lib/ui/ClimateSettingForm/ClimateSettingScheduleFormDefaultClimateSetting.js'
 import { ClimateSettingScheduleFormDeviceSelect } from 'lib/ui/ClimateSettingForm/ClimateSettingScheduleFormDeviceSelect.js'
 import { ClimateSettingScheduleFormNameAndSchedule } from 'lib/ui/ClimateSettingForm/ClimateSettingScheduleFormNameAndSchedule.js'
 import { ClimateSettingScheduleFormTimeZonePicker } from 'lib/ui/ClimateSettingForm/ClimateSettingScheduleFormTimeZonePicker.js'
@@ -55,23 +62,28 @@ export function ClimateSettingScheduleForm({
 function Content({
   onBack,
 }: Omit<ClimateSettingScheduleFormProps, 'className'>): JSX.Element {
-  const { control, watch } = useForm<ClimateSettingScheduleFormFields>({
-    defaultValues: {
-      deviceId: '',
-      name: '',
-      startDate: '',
-      endDate: '',
-      timeZone: getSystemTimeZone(),
-      climateSetting: {
-        hvacModeSetting: 'heat_cool',
-        heatingSetPoint: 70,
-        coolingSetPoint: 75,
+  const { control, watch, resetField } =
+    useForm<ClimateSettingScheduleFormFields>({
+      defaultValues: {
+        deviceId: '',
+        name: '',
+        startDate: '',
+        endDate: '',
+        timeZone: getSystemTimeZone(),
+        climateSetting: {
+          hvacModeSetting: 'heat_cool',
+          heatingSetPoint: 70,
+          coolingSetPoint: 75,
+        },
       },
-    },
-  })
+    })
 
   const deviceId = watch('deviceId')
   const timeZone = watch('timeZone')
+
+  const { device } = useDevice({
+    device_id: deviceId,
+  })
 
   const [page, setPage] = useState<
     | 'device_select'
@@ -81,13 +93,38 @@ function Content({
     | 'climate_setting'
   >('device_select')
 
+  const returnToDeviceSelect = (): void => {
+    resetField('deviceId')
+    setPage('device_select')
+  }
+
+  useEffect(() => {
+    if (page === 'device_select' && device != null) {
+      if (!isThermostatDevice(device)) return
+      const defaultSetting = device.properties.default_climate_setting
+      if (defaultSetting != null) setPage('name_and_schedule')
+      else setPage('default_setting')
+    }
+  }, [device, page, setPage])
+
   if (page === 'device_select') {
     return (
       <ClimateSettingScheduleFormDeviceSelect
         title={t.addNewClimateSettingSchedule}
         control={control}
         onBack={onBack}
-        onSelect={() => {
+      />
+    )
+  }
+
+  if (page === 'default_setting') {
+    return (
+      <ClimateSettingScheduleFormDefaultClimateSetting
+        title={t.addNewClimateSettingSchedule}
+        deviceId={deviceId}
+        onBack={returnToDeviceSelect}
+        onCancel={onBack}
+        onSave={() => {
           setPage('name_and_schedule')
         }}
       />
@@ -100,9 +137,7 @@ function Content({
         title={t.addNewClimateSettingSchedule}
         control={control}
         deviceId={deviceId}
-        onBack={() => {
-          setPage('device_select')
-        }}
+        onBack={returnToDeviceSelect}
         onCancel={onBack}
         onNext={() => {
           setPage('climate_setting')
