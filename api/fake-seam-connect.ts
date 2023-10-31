03f0164b344f35ff1e3f7c010fa1231f90c9da81
Evan Sosenko <evan@getseam.com>
2023-10-31 13:32:49 -0700

    Update fake-seam-connect.ts

--- api/fake-seam-connect.ts
+++ api/fake-seam-connect.ts
@@ -90,7 +90,7 @@ export default async (
     credentials: 'include',
     method,
     headers: reqHeaders,
-    ...(body.byteLength > 0 ? {} : { body }),
+    ...(['GET', 'HEAD', 'OPTIONS'].includes(method) ? {} : { body }),
   })
 
   const { status, headers } = proxyRes
