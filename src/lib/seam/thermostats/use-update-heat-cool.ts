189d77e3477b94affeaede67643cb72835fbf438
Tristan B. <tristan@infi.im>
2023-11-08 16:02:49 -0700

    Add `useUpdateHeatCool`

---
+++ src/lib/seam/thermostats/use-update-heat-cool.ts
@@ -0,0 +1,83 @@
+import {
+  useMutation,
+  type UseMutationResult,
+  useQueryClient,
+} from '@tanstack/react-query'
+import type {
+  SeamError,
+  ThermostatDevice,
+  ThermostatHeatCoolRequest,
+  ThermostatsListResponse,
+} from 'seamapi'
+
+import { NullSeamClientError, useSeamClient } from 'lib/seam/use-seam-client.js'
+
+// UPSTREAM: Missing ThermostatCoolResponse in seamapi.
+type UseUpdateHeatCoolData = Record<string, unknown>
+type UseUpdateHeatCoolMutationParams = ThermostatHeatCoolRequest
+
+export function useUpdateHeatCool(): UseMutationResult<
+  UseUpdateHeatCoolData,
+  SeamError,
+  UseUpdateHeatCoolMutationParams
+> {
+  const { client } = useSeamClient()
+  const queryClient = useQueryClient()
+
+  return useMutation<
+    UseUpdateHeatCoolData,
+    SeamError,
+    UseUpdateHeatCoolMutationParams
+  >({
+    mutationFn: async (mutationParams: UseUpdateHeatCoolMutationParams) => {
+      if (client === null) throw new NullSeamClientError()
+
+      return await client.thermostats.heatCool(mutationParams)
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.setQueryData<ThermostatDevice | null>(
+        ['devices', 'get', { device_id: variables.device_id }],
+        (thermostat) => {
+          if (thermostat == null) {
+            return
+          }
+
+          return getUpdatedThermostat(thermostat, variables)
+        }
+      )
+
+      queryClient.setQueryData<ThermostatsListResponse['thermostats']>(
+        ['devices', 'list', { device_id: variables.device_id }],
+        (thermostats): ThermostatDevice[] => {
+          if (thermostats == null) {
+            return []
+          }
+
+          return thermostats.map((thermostat) => {
+            if (thermostat.device_id === variables.device_id) {
+              return getUpdatedThermostat(thermostat, variables)
+            }
+
+            return thermostat
+          })
+        }
+      )
+    },
+  })
+}
+
+function getUpdatedThermostat(
+  thermostat: ThermostatDevice,
+  variables: UseUpdateHeatCoolMutationParams
+): ThermostatDevice {
+  return {
+    ...thermostat,
+    properties: {
+      ...thermostat.properties,
+      current_climate_setting: {
+        ...thermostat.properties.current_climate_setting,
+        ...variables,
+      },
+    },
+  }
+}
