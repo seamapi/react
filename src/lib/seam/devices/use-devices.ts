ff7213bffabae3a129c77d4c65778386e675dab6
Evan Sosenko <evan@getseam.com>
2023-11-09 16:34:51 -0800

    --wip-- [skip ci]

--- src/lib/seam/devices/use-devices.ts
+++ src/lib/seam/devices/use-devices.ts
@@ -1,14 +1,15 @@
 import type {
-  DevicesListBody,
+  DevicesListParams,
   DevicesListResponse,
+  SeamHttpApiError,
 } from '@seamapi/http/connect'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { useSeamClient } from 'lib/seam/use-seam-client.js'
 import type { UseSeamQueryResult } from 'lib/seam/use-seam-query-result.js'
 
-export type UseDevicesParams = DevicesListBody
-export type UseDevicesData = DevicesListResponse
+export type UseDevicesParams = DevicesListParams
+export type UseDevicesData = DevicesListResponse['devices']
 
 export function useDevices(
   params?: UseDevicesParams
@@ -16,24 +17,25 @@ export function useDevices(
   const { client } = useSeamClient()
   const queryClient = useQueryClient()
 
-  const { data, ...rest } = useQuery<DevicesListResponse['devices'], SeamError>(
-    {
-      enabled: client != null,
-      queryKey: ['devices', 'list', params],
-      queryFn: async () => {
-        if (client == null) return []
-        return await client.devices.list(params)
-      },
-      onSuccess: (devices) => {
-        for (const device of devices) {
-          queryClient.setQueryData(
-            ['devices', 'get', { device_id: device.device_id }],
-            device
-          )
-        }
-      },
-    }
-  )
+  const { data, ...rest } = useQuery<
+    DevicesListResponse['devices'],
+    SeamHttpApiError
+  >({
+    enabled: client != null,
+    queryKey: ['devices', 'list', params],
+    queryFn: async () => {
+      if (client == null) return []
+      return await client.devices.list(params)
+    },
+    onSuccess: (devices) => {
+      for (const device of devices) {
+        queryClient.setQueryData(
+          ['devices', 'get', { device_id: device.device_id }],
+          device
+        )
+      }
+    },
+  })
 
   return { ...rest, devices: data }
 }
