6bcb66fa38aa66b2630b78e9603cf55af08bf1b8
Mike Wu <hello@mikewu.dev>
2024-04-30 08:45:21 +0900

    update to verify live https://connect.getseam.com accepts image url

--- src/lib/ui/device/DeviceImage.tsx
+++ src/lib/ui/device/DeviceImage.tsx
@@ -8,20 +8,7 @@ export function DeviceImage(
 ): JSX.Element {
   const { device, ...imageProps } = props
 
-  const url = device.properties.image_url ?? fallbackImageUrl
-
-  const dbUrl =
-    'https://connect.getseam.com/internal/devicedb_image_proxy?image_id=92907fd0-a3d9-4cc8-9076-b743696d9259'
-
-  const relativePath = url.match(/assets\/(.*)$/)[1]
-  const encoded = window.encodeURIComponent(dbUrl)
-
-  const w = 128
-
-  const optimizedSrc = `http://localhost:3020/_next/image?url=${encoded}&w=${w}&q=75`
+  const optimizedSrc = `https://connect.getseam.com/_next/image?url=http%3A%2F%2Flocalhost%3A3020%2Finternal%2Fdevicedb_image_proxy%3Fimage_id%3D00e70527-3f8a-4631-ac11-c24e541e0fa5&q=75&w=128`
 
   return <img src={optimizedSrc} alt={device.properties.name} {...imageProps} />
 }
-
-const fallbackImageUrl =
-  'https://connect.getseam.com/assets/images/devices/unknown-lock.png'
