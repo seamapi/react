9c823e137a5743bc28e35aeccbc3a161af2c1c4b
Mike Wu <hello@mikewu.dev>
2024-04-30 08:50:33 +0900

    update support table ul

--- src/lib/ui/device/DeviceImage.tsx
+++ src/lib/ui/device/DeviceImage.tsx
@@ -8,7 +8,12 @@ export function DeviceImage(
 ): JSX.Element {
   const { device, ...imageProps } = props
 
-  const optimizedSrc = `https://connect.getseam.com/_next/image?url=http%3A%2F%2Flocalhost%3A3020%2Finternal%2Fdevicedb_image_proxy%3Fimage_id%3D00e70527-3f8a-4631-ac11-c24e541e0fa5&q=75&w=128`
+  const url = device.properties.image_url ?? fallbackImageUrl
+
+  const optimizedSrc = `https://connect.getseam.com/_next/image?url=${encodeURIComponent(url)}&q=75&w=128`
 
   return <img src={optimizedSrc} alt={device.properties.name} {...imageProps} />
 }
+
+const fallbackImageUrl =
+  'https://connect.getseam.com/assets/images/devices/unknown-lock.png'
