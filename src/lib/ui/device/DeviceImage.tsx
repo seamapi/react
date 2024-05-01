7b81aaa5701cd6ff9711c0fe2b3bbcd84b861342
Mike Wu <hello@mikewu.dev>
2024-05-01 18:06:49 +0900

    proxy via console

--- src/lib/ui/device/DeviceImage.tsx
+++ src/lib/ui/device/DeviceImage.tsx
@@ -10,7 +10,7 @@ export function DeviceImage(
 
   const url = device.properties.image_url ?? fallbackImageUrl
 
-  const optimizedSrc = `https://connect.getseam.com/_next/image?url=${encodeURIComponent(url)}&q=75&w=128`
+  const optimizedSrc = `https://console.seam.co/_next/image?url=${encodeURIComponent(url)}&q=75&w=128`
 
   return <img src={optimizedSrc} alt={device.properties.name} {...imageProps} />
 }
