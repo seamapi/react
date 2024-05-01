505cba94aaa464f791e80879964006827f37bc57
Evan Sosenko <evan@getseam.com>
2024-05-01 18:49:22 +0000

    Pass through class name

--- src/lib/ui/layout/DetailSection.tsx
+++ src/lib/ui/layout/DetailSection.tsx
@@ -1,19 +1,22 @@
 import type { PropsWithChildren } from 'react'
 
 import { Tooltip } from 'lib/ui/Tooltip/Tooltip.js'
+import classNames from 'classnames'
 
 interface DetailSectionProps {
   label?: string
   tooltipContent?: string
+  className?: string
 }
 
 export function DetailSection({
   label,
   tooltipContent,
+  className,
   children,
 }: PropsWithChildren<DetailSectionProps>): JSX.Element {
   return (
-    <div className='seam-detail-section'>
+    <div className={classNames('seam-detail-section', className)}>
       <div className='seam-detail-label-wrap'>
         {label != null && <p className='seam-detail-label'>{label}</p>}
         {tooltipContent != null && <Tooltip>{tooltipContent}</Tooltip>}
