396ba056583730b9cc077bdb5ef43f39a3715d09
Evan Sosenko <evan@getseam.com>
2024-05-03 12:03:20 -0700

    feat: Add className to device info section (#629)
    * feat: Add className to DeviceInfo
    
    * Pass through class name
    
    * ci: Format code
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>

--- src/lib/ui/layout/DetailSection.tsx
+++ src/lib/ui/layout/DetailSection.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import type { PropsWithChildren } from 'react'
 
 import { Tooltip } from 'lib/ui/Tooltip/Tooltip.js'
@@ -5,15 +6,17 @@ import { Tooltip } from 'lib/ui/Tooltip/Tooltip.js'
 interface DetailSectionProps {
   label?: string
   tooltipContent?: JSX.Element | string
+  className?: string
 }
 
 export function DetailSection({
   label,
   tooltipContent,
+  className,
   children,
 }: PropsWithChildren<DetailSectionProps>): JSX.Element {
   return (
-    <div className='seam-detail-section'>
+    <div className={classNames('seam-detail-section', className)}>
       <div className='seam-detail-label-wrap'>
         {label != null && <p className='seam-detail-label'>{label}</p>}
         {tooltipContent != null && <Tooltip>{tooltipContent}</Tooltip>}
