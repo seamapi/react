cb0d044f0768bc9528eb43722872d61699b16cc5
Tristan B. <tristan@infi.im>
2024-05-16 15:37:22 -0600

    Fix merge issues

--- src/lib/ui/noise-sensor/NoiseSensorActivityList.tsx
+++ src/lib/ui/noise-sensor/NoiseSensorActivityList.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { useEvents } from 'lib/seam/events/use-events.js'
 import { NoiseSensorEventItem } from 'lib/ui/noise-sensor/NoiseSensorEventItem.js'
-=======
->>>>>>> 4393fa0eb9f4d55fa0ab31fb392bc6f53df5f5ae
 import type { NoiseSensorDevice } from 'seamapi'
 
 import { useEvents } from 'lib/seam/events/use-events.js'
