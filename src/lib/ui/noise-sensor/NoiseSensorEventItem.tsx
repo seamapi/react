beac82685e75dcfa3674cc3edecc38bb01685888
Tristan B. <tristan@infi.im>
2024-05-17 13:57:12 -0600

    Update types and row layout style

--- src/lib/ui/noise-sensor/NoiseSensorEventItem.tsx
+++ src/lib/ui/noise-sensor/NoiseSensorEventItem.tsx
@@ -24,9 +24,50 @@ export function NoiseSensorEventItem({
       </div>
 
       <div className='seam-noise-sensor-event-item-context-wrap'>
-        <p className='seam-noise-sensor-event-item-context-label'>{`Noisy (${event.noise_level_decibels} dB)`}</p>
-        <p className='seam-noise-sensor-event-item-context-sublabel'>Kitchen</p>
+        <p className='seam-noise-sensor-event-item-context-label'>
+          {getContextLabel(event)}
+        </p>
+        {getContextSublabel(event) && (
+          <p className='seam-noise-sensor-event-item-context-sublabel'>
+            {getContextSublabel(event)}
+          </p>
+        )}
       </div>
+
+      <div className='seam-noise-sensor-event-item-right-block'></div>
     </div>
   )
+}
+
+function getContextLabel(event: Event): string {
+  switch (event.event_type) {
+    case 'noise_sensor.noise_threshold_triggered':
+      return `${t.noiseThresholdTriggered} ${'noise_threshold_name' in event && `(${event.noise_threshold_name})`}`
+    case 'noise_detection.detected_noise':
+      return `${t.detectedNoise} ${'noise_level_decibels' in event ? `(${event.noise_level_decibels} ${t.decibel})` : ''}`
+    default:
+      return t.noiseDetected
+  }
+}
+
+function getContextSublabel(event: Event): string {
+  if ('noise_level_decibels' in event) {
+    return `${event.noise_level_decibels} ${t.decibel}`
+  }
+
+  if (
+    'noise_threshold_name' in event &&
+    typeof event.noise_threshold_name === 'string'
+  ) {
+    return event.noise_threshold_name
+  }
+
+  return ''
+}
+
+const t = {
+  detectedNoise: 'Noise detected',
+  decibel: 'dB',
+  noiseThresholdTriggered: 'Noise threshold triggered',
+  noiseDetected: 'Noise detected',
 }
