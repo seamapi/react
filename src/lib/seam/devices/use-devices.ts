c0f761c505dfd9c31eff9e1ddbb969b07eb67586
Evan Sosenko <evan@getseam.com>
2024-06-27 16:52:00 -0700

    Update useDevices hook

--- src/lib/seam/devices/use-devices.ts
+++ src/lib/seam/devices/use-devices.ts
@@ -1,16 +1,12 @@
+import type { DevicesListParams, SeamHttpApiError } from '@seamapi/http/connect'
+import type { Device } from '@seamapi/types/connect'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
-import type {
-  CommonDevice,
-  DevicesListRequest,
-  DevicesListResponse,
-  SeamError,
-} from 'seamapi'
 
 import { useSeamClient } from 'lib/seam/use-seam-client.js'
 import type { UseSeamQueryResult } from 'lib/seam/use-seam-query-result.js'
 
-export type UseDevicesParams = DevicesListRequest
-export type UseDevicesData = CommonDevice[]
+export type UseDevicesParams = DevicesListParams
+export type UseDevicesData = Device[]
 
 export function useDevices(
   params?: UseDevicesParams
@@ -18,23 +14,21 @@ export function useDevices(
   const { client } = useSeamClient()
   const queryClient = useQueryClient()
 
-  const { data, ...rest } = useQuery<DevicesListResponse['devices'], SeamError>(
-    {
-      enabled: client != null,
-      queryKey: ['devices', 'list', params],
-      queryFn: async () => {
-        if (client == null) return []
-        const devices = await client.devices.list(params)
-        for (const device of devices) {
-          queryClient.setQueryData(
-            ['devices', 'get', { device_id: device.device_id }],
-            device
-          )
-        }
-        return devices
-      },
-    }
-  )
+  const { data, ...rest } = useQuery<UseDevicesData, SeamHttpApiError>({
+    enabled: client != null,
+    queryKey: ['devices', 'list', params],
+    queryFn: async () => {
+      if (client == null) return []
+      const devices = await client.devices.list(params)
+      for (const device of devices) {
+        queryClient.setQueryData(
+          ['devices', 'get', { device_id: device.device_id }],
+          device
+        )
+      }
+      return devices
+    },
+  })
 
   return { ...rest, devices: data }
 }
