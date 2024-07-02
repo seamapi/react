24b441e068bd51c06a867c03b1f58f95ce20053f
Evan Sosenko <evan@getseam.com>
2024-07-02 23:48:50 +0000

    Update useCreateClimateSettingSchedule

--- src/lib/seam/thermostats/climate-setting-schedules/use-create-climate-setting-schedule.ts
+++ src/lib/seam/thermostats/climate-setting-schedules/use-create-climate-setting-schedule.ts
@@ -1,44 +1,50 @@
+import type {
+  SeamHttpApiError,
+  ThermostatsClimateSettingSchedulesCreateBody,
+} from '@seamapi/http/connect'
+import type { ClimateSettingSchedule } from '@seamapi/types/connect'
 import {
   useMutation,
   type UseMutationResult,
   useQueryClient,
 } from '@tanstack/react-query'
-import type {
-  ClimateSettingSchedule,
-  ClimateSettingScheduleCreateRequest,
-  ClimateSettingScheduleCreateResponse,
-  SeamError,
-} from 'seamapi'
 
 import { NullSeamClientError, useSeamClient } from 'lib/seam/use-seam-client.js'
+
+export type UseCreateClimateSettingScheduleParams = never
 
 export type UseCreateClimateSettingScheduleData = ClimateSettingSchedule
 
 export type UseCreateClimateSettingScheduleMutationVariables =
-  ClimateSettingScheduleCreateRequest
+  ThermostatsClimateSettingSchedulesCreateBody
 
 export function useCreateClimateSettingSchedule(): UseMutationResult<
   UseCreateClimateSettingScheduleData,
-  SeamError,
+  SeamHttpApiError,
   UseCreateClimateSettingScheduleMutationVariables
 > {
   const { client } = useSeamClient()
   const queryClient = useQueryClient()
 
   return useMutation<
-    ClimateSettingScheduleCreateResponse['climate_setting_schedule'],
-    SeamError,
-    ClimateSettingScheduleCreateRequest
+    UseCreateClimateSettingScheduleData,
+    SeamHttpApiError,
+    UseCreateClimateSettingScheduleMutationVariables
   >({
-    mutationFn: async (
-      variables: UseCreateClimateSettingScheduleMutationVariables
-    ) => {
+    mutationFn: async (variables) => {
       if (client === null) throw new NullSeamClientError()
-      const result =
-        await client.thermostats.climateSettingSchedules.create(variables)
-      return result
+      return await client.thermostats.climateSettingSchedules.create(variables)
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
+      queryClient.setQueryData(
+        [
+          'thermostats',
+          'climate_setting_schedules',
+          'get',
+          { climate_setting_schedules: data.climate_setting_schedule_id },
+        ],
+        data
+      )
       void queryClient.invalidateQueries({
         queryKey: ['thermostats', 'climate_setting_schedules', 'list'],
       })
