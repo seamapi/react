6b7777eee7e0392c7f1342c2079701c6efc87837
Evan Sosenko <evan@getseam.com>
2024-07-02 23:53:26 +0000

    Update useDeleteClimateSettingSchedule

--- src/lib/seam/thermostats/climate-setting-schedules/use-delete-climate-setting-schedule.ts
+++ src/lib/seam/thermostats/climate-setting-schedules/use-delete-climate-setting-schedule.ts
@@ -1,25 +1,22 @@
+import type { SeamHttpApiError, ThermostatsClimateSettingSchedulesDeleteParams } from '@seamapi/http/connect'
 import {
   useMutation,
   type UseMutationResult,
   useQueryClient,
 } from '@tanstack/react-query'
-import type {
-  ClimateSettingScheduleDeleteRequest,
-  ClimateSettingScheduleDeleteResponse,
-  SeamError,
-} from 'seamapi'
 
 import { NullSeamClientError, useSeamClient } from 'lib/seam/use-seam-client.js'
 
-export type UseDeleteClimateSettingScheduleMutationVariables =
-  ClimateSettingScheduleDeleteRequest
+export type UseDeleteClimateSettingScheduleParams = never
 
-export type UseDeleteClimateSettingScheduleData =
-  ClimateSettingScheduleDeleteResponse
+export type UseDeleteClimateSettingScheduleData = undefined
+
+export type UseDeleteClimateSettingScheduleMutationVariables =
+  ThermostatsClimateSettingSchedulesDeleteParams
 
 export function useDeleteClimateSettingSchedule(): UseMutationResult<
   UseDeleteClimateSettingScheduleData,
-  SeamError,
+  SeamHttpApiError,
   UseDeleteClimateSettingScheduleMutationVariables
 > {
   const { client } = useSeamClient()
@@ -27,12 +24,10 @@ export function useDeleteClimateSettingSchedule(): UseMutationResult<
 
   return useMutation<
     UseDeleteClimateSettingScheduleData,
-    SeamError,
-    ClimateSettingScheduleDeleteRequest
+    SeamHttpApiError,
+    UseDeleteClimateSettingScheduleMutationVariables
   >({
-    mutationFn: async (
-      variables: UseDeleteClimateSettingScheduleMutationVariables
-    ) => {
+    mutationFn: async (variables) => {
       if (client === null) throw new NullSeamClientError()
       await client.thermostats.climateSettingSchedules.delete(variables)
     },
@@ -42,9 +37,7 @@ export function useDeleteClimateSettingSchedule(): UseMutationResult<
           'thermostats',
           'climate_setting_schedules',
           'get',
-          {
-            climate_setting_schedule_id: variables.climate_setting_schedule_id,
-          },
+          { climate_setting_schedules: variables.climate_setting_schedule_id },
         ],
       })
       void queryClient.invalidateQueries({
