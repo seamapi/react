f5a1b311df3e81dbd76b9faf96ec7bf46e2f851a
Evan Sosenko <evan@getseam.com>
2024-07-19 15:02:21 +0000

    Update src/lib/object.ts
    Co-authored-by: Charles Pick <charles@codemix.com>

--- src/lib/object.ts
+++ src/lib/object.ts
@@ -1,4 +1,5 @@
-export const shake = <T>(obj: T): Partial<T> => {
+type Shook<T> = {[K in keyof T as T[K] extends undefined ? never : K]: T[K]}
+export const shake = <T>(obj: T): Shook<T> => {
   if (obj == null) return obj
   if (typeof obj !== 'object') return obj
   return Object.entries(obj).reduce((acc, [k, v]) => {
