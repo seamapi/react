dc8fc1cb1ec8a8a33d8bd0c670cc0922fd1ecab5
Evan Sosenko <evan@getseam.com>
2024-07-19 16:17:28 +0000

    Fix Shook type error

--- src/lib/object.ts
+++ src/lib/object.ts
@@ -1,9 +1,12 @@
-type Shook<T> = { [K in keyof T as T[K] extends undefined ? never : K]: T[K] }
 export const shake = <T>(obj: T): Shook<T> => {
   if (obj == null) return obj
   if (typeof obj !== 'object') return obj
-  return Object.entries(obj).reduce((acc, [k, v]) => {
+
+  const initialValue = {} as Shook<T>
+  return Object.entries(obj).reduce<Shook<T>>((acc, [k, v]) => {
     if (v === undefined) return acc
     return { ...acc, [k]: v }
-  }, {})
+  }, initialValue)
 }
+
+type Shook<T> = { [K in keyof T as T[K] extends undefined ? never : K]: T[K] }
