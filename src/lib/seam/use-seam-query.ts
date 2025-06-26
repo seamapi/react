a85bfde34736db76ee6cb1298c6a0ad717312162
Evan Sosenko <evan@getseam.com>
2025-06-26 00:21:21 -0700

    Use SeamHttpEndpointQueryPaths

--- src/lib/seam/use-seam-query.ts
+++ src/lib/seam/use-seam-query.ts
@@ -1,27 +1,26 @@
 import type {
   SeamHttpApiError,
-  SeamHttpEndpointPaths,
+  SeamHttpEndpointQueryPaths,
   SeamHttpEndpoints,
 } from '@seamapi/http/connect'
 import { useQuery, type UseQueryResult } from '@tanstack/react-query'
 
 import { useSeamClient } from 'lib/seam/use-seam-client.js'
 
-type Endpoints = Pick<SeamHttpEndpoints, SeamHttpEndpointPaths>
-
-export function useSeamQuery<T extends keyof Endpoints>(
+export function useSeamQuery<T extends SeamHttpEndpointQueryPaths>(
   endpointPath: T,
-  params?: Parameters<Endpoints[T]>[0],
-  options?: Parameters<Endpoints[T]>[1]
-): UseQueryResult<Awaited<ReturnType<Endpoints[T]>>, SeamHttpApiError> {
+  params?: Parameters<SeamHttpEndpoints[T]>[0],
+  options?: Parameters<SeamHttpEndpoints[T]>[1]
+): UseQueryResult<Awaited<ReturnType<SeamHttpEndpoints[T]>>, SeamHttpApiError> {
   const { endpointClient: client } = useSeamClient()
   return useQuery({
     enabled: client != null,
     queryKey: [endpointPath, params],
     queryFn: async () => {
       if (client == null) return null
-      const endpoint = client[endpointPath] as Endpoints[T]
-      // @ts-expect-error: The types are correct at runtime, but TypeScript can't infer the specific endpoint types
+      // Using @ts-expect-error over any is preferred, but not possible here because TypeScript will run out of memory.
+      // Type assertion is needed here for performance reasons. The types are correct at runtime.
+      const endpoint = client[endpointPath] as any
       return await endpoint(params, options)
     },
   })
