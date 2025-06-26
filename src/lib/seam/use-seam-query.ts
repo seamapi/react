a981f99ce7bf4b3228c65448fd28b142ff2cd0cc
Evan Sosenko <evan@getseam.com>
2025-06-26 00:27:04 -0700

    Rename params to parameters

--- src/lib/seam/use-seam-query.ts
+++ src/lib/seam/use-seam-query.ts
@@ -9,19 +9,19 @@ import { useSeamClient } from 'lib/seam/use-seam-client.js'
 
 export function useSeamQuery<T extends SeamHttpEndpointQueryPaths>(
   endpointPath: T,
-  params?: Parameters<SeamHttpEndpoints[T]>[0],
+  parameters?: Parameters<SeamHttpEndpoints[T]>[0],
   options?: Parameters<SeamHttpEndpoints[T]>[1]
 ): UseQueryResult<Awaited<ReturnType<SeamHttpEndpoints[T]>>, SeamHttpApiError> {
   const { endpointClient: client } = useSeamClient()
   return useQuery({
     enabled: client != null,
-    queryKey: [endpointPath, params],
+    queryKey: [endpointPath, parameters],
     queryFn: async () => {
       if (client == null) return null
       // Using @ts-expect-error over any is preferred, but not possible here because TypeScript will run out of memory.
       // Type assertion is needed here for performance reasons. The types are correct at runtime.
       const endpoint = client[endpointPath] as any
-      return await endpoint(params, options)
+      return await endpoint(parameters, options)
     },
   })
 }
