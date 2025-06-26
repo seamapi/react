3bda3f4d0fba53151fbd3141e2a3b5911701306e
Evan Sosenko <evan@getseam.com>
2025-06-25 18:06:42 -0700

    feat: Add useSeamQuery

---
+++ src/lib/seam/use-seam-query.ts
@@ -0,0 +1,27 @@
+import type { SeamHttpApiError, SeamHttpEndpoints } from '@seamapi/http/connect'
+import { useQuery, type UseQueryResult } from '@tanstack/react-query'
+
+import { useSeamClient } from 'lib/seam/use-seam-client.js'
+
+type Endpoints = Omit<
+  SeamHttpEndpoints,
+  Exclude<keyof SeamHttpEndpoints, `/${string}`>
+>
+
+export function useSeamQuery<T extends keyof Endpoints>(
+  endpointPath: T,
+  params?: Parameters<Endpoints[T]>[0],
+  options?: Parameters<Endpoints[T]>[1]
+): UseQueryResult<Awaited<ReturnType<Endpoints[T]>>, SeamHttpApiError> {
+  const { endpointClient: client } = useSeamClient()
+  return useQuery({
+    enabled: client != null,
+    queryKey: [endpointPath, params],
+    queryFn: async () => {
+      if (client == null) return null
+      const endpoint = client[endpointPath] as Endpoints[T]
+      // @ts-expect-error: The types are correct at runtime, but TypeScript can't infer the specific endpoint types
+      return await endpoint(params, options)
+    },
+  })
+}
